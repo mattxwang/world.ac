01872c102ec84964e67436dfc126c464e13d66ca
malsf21 <matthewang.wang@gmail.com>
2015-10-15 14:40:25 -0400

    try out pills

--- test.php
+++ test.php
@@ -82,6 +82,12 @@ include_once("../includes/sql.php");
   </div>
 </nav>
 
+
+<ul class="nav nav-pills">
+  <li role="presentation" class="active"><a href="#">Home</a></li>
+  <li role="presentation"><a href="#">Profile</a></li>
+  <li role="presentation"><a href="#">Messages</a></li>
+</ul>
 </br>
 </br>
 </br>
