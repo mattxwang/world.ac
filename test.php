eec9b3e902671bb6def4d08ca3e2b86b78e17785
malsf21 <matthewang.wang@gmail.com>
2015-10-15 10:48:45 -0400

    added another dropdown
    swag

--- test.php
+++ test.php
@@ -92,6 +92,21 @@ include_once("../includes/sql.php");
         	-->
           </ul>
         </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Conference Information <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Keynote Address</a></li>
+            <li><a href="#">Morning Panel</a></li>
+            <li><a href="#">Plenaries</a></li>
+            <li><a href="#">Closing Panel</a></li>
+            <!--
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+        	-->
+          </ul>
+        </li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
