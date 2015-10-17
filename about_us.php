ba5041f9727cbaee270a43f6f1635880eff6e09d
Matthew Wang <matthewang.wang@gmail.com>
2015-10-17 18:08:33 -0400

    swap positioning

--- about_us.php
+++ about_us.php
@@ -96,8 +96,8 @@ include_once("../includes/sql.php");
 	        </ol>
 	        <div class="carousel-mod carousel-inner" role="listbox">
 	          <div class="carousel-mod item active">
+	          	<img src="/img/bio/mwang.png" alt="Matthew Wang">
 	           	<span style="position:absolute; bottom:0px; left:0px; width:100%; height:100%; background-color:#DB0C0A; opacity:1;" class="hidden-xs"></span>
-	            <img src="/img/bio/mwang.png" alt="Matthew Wang">
 	          </div>
 	          <div class="carousel-mod item">
 	            <img src="/img/bio/jsarick.png" alt="Jack Sarick">
