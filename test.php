3bb7196c0fe6ded50e8730465e7209a0180d0b84
Matthew Wang <matthewang.wang@gmail.com>
2015-10-17 17:14:00 -0400

    mobile navbar bugfix

--- test.php
+++ test.php
@@ -133,7 +133,7 @@ elseif($rando == 2){
   <div class="container-fluid navbar-footer" style ="text-algin:center;">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -141,7 +141,7 @@ elseif($rando == 2){
       </button>
       <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt = "World Affairs Conference" height="25px"></img></a>
     </div>
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
       <ul class="nav navbar-nav">
         <li><a href="about_us.php">WAC TEAM</a></li>
         <li><a href="#">PLENARIES</a></li>
