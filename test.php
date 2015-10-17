5fee193533adb54f15993ffad752afefb0424cdd
Matthew Wang <matthewang.wang@gmail.com>
2015-10-17 17:48:38 -0400

    renamed class

--- test.php
+++ test.php
@@ -64,7 +64,7 @@ include_once("../includes/sql.php");
 		}
 		window.onload=function() {
 			if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-				$(".navbar_active").remove();
+				$(".border_bottom").remove();
 			}
 			$("#bg_stars").height($(window).outerHeight(true)+$("#second").outerHeight(true));
 			window.onscroll = function() {
@@ -95,7 +95,7 @@ include_once("../includes/sql.php");
         <li><a href="#">Registration</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Conference Information <span class="caret"></span></a>
-          <span style="position:absolute; bottom:-1px; left:0px; width:100%; background-color:#53b5b4; height:4px; opacity:1;" class="navbar_active"></span>
+          <span style="position:absolute; bottom:-1px; left:0px; width:100%; background-color:#53b5b4; height:4px; opacity:1;" class="border_bottom"></span>
           <ul class="dropdown-menu">
             <li><a href="keynote.php">Keynote Address</a></li>
             <li class ="active"><a href="#">Morning Panel</a></li>
