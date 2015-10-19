7b552377491bee36f1a1483ae8c48fba292b0a06
malsf21 <matthewang.wang@gmail.com>
2015-10-19 15:20:00 -0400

    transparency?

--- footer.php
+++ footer.php
@@ -32,8 +32,8 @@ elseif($rando == 2){
 
 ?>
 
-<nav class="navbar navbar-default navbar-fixed-bottom navbar-inverse navbar-footer hidden-xs">
-  <div class="container-fluid navbar-footer" style ="text-algin:center;background-color:rgba(0,0,0,0.4)">
+<nav class="navbar navbar-default navbar-fixed-bottom navbar-inverse navbar-footer hidden-xs" style="background-color:transparent; ">
+  <div class="container-fluid navbar-footer" style ="text-algin:center">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2" aria-expanded="false">
