2f5e7371788f425650a61efd79207660503e7f6e
Matthew Wang <matthewang.wang@gmail.com>
2015-10-19 00:04:58 -0400

    one mroe try

--- test.php
+++ test.php
@@ -23,6 +23,39 @@ include_once("../includes/sql.php");
 	<link rel="stylesheet" href="css/base.css" />
 	<link href="css/custom.css" rel="stylesheet">
 	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script>
+	// set the date we're counting down to
+	var target_date = new Date("Aug 15, 2019").getTime();
+	 
+	// variables for time units
+	var days, hours, minutes, seconds;
+	 
+	// get tag element
+	var countdown = document.getElementById("countdown");
+	 
+	// update the tag with id "countdown" every 1 second
+	setInterval(function () {
+	 
+	    // find the amount of "seconds" between now and target
+	    var current_date = new Date().getTime();
+	    var seconds_left = (target_date - current_date) / 1000;
+	 
+	    // do some time calculations
+	    days = parseInt(seconds_left / 86400);
+	    seconds_left = seconds_left % 86400;
+	     
+	    hours = parseInt(seconds_left / 3600);
+	    seconds_left = seconds_left % 3600;
+	     
+	    minutes = parseInt(seconds_left / 60);
+	    seconds = parseInt(seconds_left % 60);
+	     
+	    // format countdown string + set tag value
+	    countdown.innerHTML = days + "d, " + hours + "h, "
+	    + minutes + "m, " + seconds + "s";  
+	 
+	}, 1000);
+	</script>
 </head>
 
 <body style="position:relative;">
@@ -30,41 +63,7 @@ include_once("../includes/sql.php");
 include_once('navbar.php');
 ?>
 
-<script>
-// set the date we're counting down to
-var target_date = new Date("Aug 15, 2019").getTime();
- 
-// variables for time units
-var days, hours, minutes, seconds;
- 
-// get tag element
-var countdown = document.getElementById("countdown");
- 
-// update the tag with id "countdown" every 1 second
-setInterval(function () {
- 
-    // find the amount of "seconds" between now and target
-    var current_date = new Date().getTime();
-    var seconds_left = (target_date - current_date) / 1000;
- 
-    // do some time calculations
-    days = parseInt(seconds_left / 86400);
-    seconds_left = seconds_left % 86400;
-     
-    hours = parseInt(seconds_left / 3600);
-    seconds_left = seconds_left % 3600;
-     
-    minutes = parseInt(seconds_left / 60);
-    seconds = parseInt(seconds_left % 60);
-     
-    // format countdown string + set tag value
-    countdown.innerHTML = days + "d, " + hours + "h, "
-    + minutes + "m, " + seconds + "s";  
- 
-}, 1000);
-</script>
-
-<span id="countdown"></span>
+<span id="countdown"> until swiggity swoogity</span>
 <?php
 include_once("footer.php");
 ?>
