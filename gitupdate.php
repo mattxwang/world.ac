a5c4533f59dc64d0cda55fc6095d111b85e98f08
malsf21 <matthewang.wang@gmail.com>
2015-10-19 16:54:21 -0400

    muahahaha webhooks

---
+++ gitupdate.php
@@ -0,0 +1,10 @@
+<h1>Pulling <a href="https://github.com/malsf21/world.ac">https://github.com/malsf21/world.ac</a> to server</h1>
+<?php
+	exec("git pull devel master", $output);
+	if (count($output) == 1){
+		echo $output[0];
+	}
+	else {
+		echo $output[3];
+	}
+?>
---
+++ pullupdate.sh
@@ -0,0 +1,3 @@
+git pull &>errors.txt;
+echo '<h2>Done!</h2>';
+echo '<h2>Done!</h2>' >> errors.txt;
