3b426a212f781ad4ba34f4e322fc0d04cfffc729
Matthew Wang <matthewang.wang@gmail.com>
2015-10-21 21:16:23 -0400

    tried sidewinding fb

--- contactus.php
+++ contactus.php
@@ -62,11 +62,11 @@ include_once("../includes/sql.php");
 	</div>
 	<div class="col-md-2">
 		<h1>Social Media</h1>
-		<h3>Facebook Page</h3>
 		<div style="width:30%">
 		<a href="https://www.facebook.com/worldaffairsconference?fref=ts">
 		<img src="img/facebook.png" class="img-responsive"></img>
 		</a>
+		<h3 style="vertical-align:middle;">Facebook Page</h3>
 		</div>
 	</div>
 	<div class="col-md-2">
