9eaf70efb4e4f11002806934c55f8e42ffbfda93
malsf21 <matthewang.wang@gmail.com>
2015-10-22 10:49:13 -0400

    removed all breaks

--- faq_test.php
+++ faq_test.php
@@ -54,15 +54,15 @@ include_once("../includes/sql.php");
 		<p> The World Affairs Conference (WAC) is Canada's oldest annual student-run current affairs conference. Annually held at Upper Canada College in Toronto, Canada, the World Affairs Conference connects over 800 students, with a common interest in current affairs, from across North America. A dedicated team of about 50 high school students from Upper Canada College, in coordination with Branksome Hall, voluntarily contribute their time for many months leading up to the day of WAC to ensure the success and positive experience for each delegate that attends the conference.</p>
 	</div>
 	<div id = "gen_date">
-		<h3>2016 dates</h3></br>
-		<p> We haven't quite decided the exact date of WAC. More information will be known as registration packages get sent out. </p></br>
+		<h3>2016 dates</h3>
+		<p> We haven't quite decided the exact date of WAC. More information will be known as registration packages get sent out. </p>
 	</div>
 	<div id = "gen_days">
-		<h3>Do students attend both days?</h3></br>
+		<h3>Do students attend both days?</h3>
 		<p>The first day of the conference consists just of the Lionel Gelber Keynote Address which is hosted in the evening. This event is open to the Toronto community, and anyone is welcome to attend. The second day of the conference begins at 8AM and is attended by only registered student delegates and their faculty supervisors. We do encourage students to attend the Keynote Address if it is of interest to them. It tends to feature a well know speaker of great importance to current affairs.</p>
 	</div>
 	<div id = "media">
-		<h3>Media information</h3></br>
+		<h3>Media information</h3>
 		<p>For any information regarding media coverage please contact the Head of the Department of Registration and Media: Emmanuel Wong (emmanuel.wong@ucc.on.ca). If you require any WAC branding for your work please feel free to use the following provided logos: <a href="misc_files/WAC_Logos.zip"><b>download here.</b></a></p>
 	</div>
 
@@ -71,17 +71,17 @@ include_once("../includes/sql.php");
 	</div>
 	<div id="reg_student_cost" style="font-size:20px;font-weight:bold;">
 	<p>Registration for WAC 2016 is being organized right now. Updates will be sent out shortly.</p>
-	</div></br>
+	</div>
 	<div id = "how_to_reg">
-		<h3>How can I register my school?</h3></br>
+		<h3>How can I register my school?</h3>
 		<p>To attend the world affairs conference you must submit your registration form and means of payment by the registration deadline. <!-- The registration information package, as well as the forms required to register are available for download, please click the blue 'register' button at the top right of the page to view the PDF file. Please note the forms are located at the end of this file, which also includes the registration information package.--></p>
 	</div>
 	<div id = "reg_who">
-		<h3>Who can attend WAC?</h3></br>
+		<h3>Who can attend WAC?</h3>
 		<p>WAC is open to anyone is currently in grades 9 to 12 with an interest in politics or current affairs. </p>
 	</div>
 	<div id = "reg_due">
-		<h3>When is the registration due?</h3></br>
+		<h3>When is the registration due?</h3>
 		<p>This, along with other information, will be sent out as we finish the registration package. </p>
 		<!--
 		<p>The registration is due by mail on December 16th 2014. We may not be able to accept late registrants so please ensure your forms are submitted promptly. </h3>
@@ -92,15 +92,15 @@ include_once("../includes/sql.php");
 	<h2>Conference Day</h2>
 	</div>
 	<div id = "theday">
-		<h3>What can I expect on the day of WAC?</h3></br>
+		<h3>What can I expect on the day of WAC?</h3>
 		<p>School supervisors and their students are encouraged to arrive at or before 8AM for the registration check in process. Each student will receive their personal package with their name tag. The conference will then be kicked off with our morning panel. The closing ceremonies typically end at around 4:40PM</p>
 	</div>
 	<div id = "lunch">
-		<h3>Are students provided lunch?</h3></br>
+		<h3>Are students provided lunch?</h3>
 		<p>Yes students are provided lunch free of charge on the day of the conference.</p>
 	</div>
 	<div id = "allergy">
-		<h3>How do I notify WAC of allergies?</h3></br>
+		<h3>How do I notify WAC of allergies?</h3>
 		<p>If you would like to notify us of a food allergy that we should be aware of, please include any details we will need on a separate piece of paper within your return envelope.</p>
 	</div>
 </div>
