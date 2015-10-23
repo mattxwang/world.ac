f5dfad13aa2b0093e2d321f3b86235503e6afef1
malsf21 <matthewang.wang@gmail.com>
2015-10-23 09:45:40 -0400

    pushes test_ changes to index
    Changelog:
    Padding, layout and font sizes are now adapted to screen width and
    viewport size: mobile text-stacking issues are fixed
    
    Imports js file for countdown; fixes sketchy countdown code into simple
    10-line solution
    
    Redesigns table layout for the ABOUT WAC section, using Bootstrap’s
    Grid System
    
    Adapts more of index to mobile devices

--- index.php
+++ index.php
@@ -29,81 +29,63 @@ $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 	<link href="css/bootstrap/bootstrap.css" rel="stylesheet">
 	<link href="css/bootstrap/bootstrap-theme.css" rel="stylesheet">
 	<link rel="stylesheet" href="css/base.css" />
-	<link href="css/custom.css" rel="stylesheet">
-	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script src="js/countdown.js"></script>
 	<script>
 	if (!window.jQuery) {
 	    document.write('<script src="js/jquery.js"><\/script>');
 	}
 	</script>
 	<script type="text/javascript" src="js/easing.js"></script>
-	<script type="text/javascript">
-	window.onresize = function() {
-		$("#bg_stars").height($(window).outerHeight(true)+$("#second").outerHeight(true));
-			document.getElementById("bg_earth").style.top = (-($(document).scrollTop()/($("body").height()-$(window).height()))*$("#bg_earth").height()*0.08)+"px";
-			//$("#bg_stars>div").css("top", (-($(document).scrollTop()/($("body").height()-$(window).height()))*$("#bg_stars").height()*0.09));
-	}
-	window.onload=function() {
-		$("#bg_stars").height($(window).outerHeight(true)+$("#second").outerHeight(true));
-		window.onscroll = function() {
-			document.getElementById("bg_earth").style.top = (-($(document).scrollTop()/($(window).height()-$(document).height()))*$("#bg_earth").height()*0.35)+"px";
-			//console.log($("body").height()+"|"+$(document).height());
-			//$("#bg_stars>div").css("top", (-($(document).scrollTop()/($(window).height()-$(document).height()))*$("#bg_stars").height()*0.4));
-		}
-	}
-	</script>
 </head>
 
 <!-- || Main Page Content || -->
 <body>
 <?php include_once("tracking.php")  ?>
-<div class="container-fluid">
 
-<div id="bg_stars"></div><div></div><div id="bg_earth"></div>
+<div id="bg_stars"><div></div></div><div id="bg_earth"></div>
 
 <?php
 include_once("navbar.php")  ?>
 
-<div style="font-family:Nexa; line-height:80%; color:#FFF; font-size:70px; margin:0 10px 0 10px; margin-top:50px; position:relative;"><!-- to be replaced with a jQuery automated solution -->
-	WELCOME TO THE 2016<div style="font-weight:bold;">WORLD AFFAIRS CONFERENCE.</div>
-	<!--
-	<div style="position:absolute; right:0px; top:0px;">
-		
-		<span style="font-family:DIN; font-size:12px; float:right;">
-			<a href="/live/" target="_blank"><span style="position:relative; line-height:16px; vertical-align:middle; cursor:pointer;">
-				<span style="background-color:#4c9190;" class="under_button round"></span>
-				<span style="background-color:#53b5b4; color:#FFF; font-weight:bold;" class="button round">LIVESTREAM</span>
-			</span></a>
-			<a href="/misc_files/del.pdf"><span style="position:relative; margin-left:10px; line-height:16px; vertical-align:middle; cursor:pointer;">
-				<span style="background-color:#4c9190;" class="under_button round"></span>
-				<span style="background-color:#53b5b4; color:#FFF; font-weight:bold;" class="button round">DELEGATE PACKAGE</span>
-			</span></a>
-		</span>
-		
+<div style="font-family:Nexa; line-height:80%; color:#FFF; padding:4%; padding-top:5%; position:relative">
+	<div style="font-size:5vw;">
+	WELCOME TO THE 2016 <div style="font-weight:bold; padding-top: 5vw;">WORLD AFFAIRS CONFERENCE.</div>
 	</div>
-<br />
-
-<div style="font-size:50px; "><b>2015 Theme:</b> <span style="font-size:40;">THREATS TO HUMANITY</span></div>
-<div style="font-size:50px; "><b>This Year's Keynote Speaker:</b> <span style="font-size:40;">Edward Snowden</span></div>
--->
+	<div style="padding:0 10px 0 10px; padding-top:5vw; font-size:3vw; text-align:right"> More Information In: <div id="timeLeft" style="padding-top:3vw;">00 Days 00 Hours 00 Minutes and 00 Seconds</div></div>
+	<script type="text/javascript">
+		// I tried to make it as simple as possible
+		var deadLine = 'November 18 2015';
+		window.setInterval(function(){
+  			// console.log(getTimeRemaining(deadLine));
+  			$("#timeLeft").html(getTimeRemaining(deadLine));
+		}, 1000);
+	</script>
 
 </div>
-
-<div style="font-family:Nexa; letter-spacing:5px; color:#FFF; margin-top:50px; position:absolute; bottom:30px; left:0px; width:100%;">
-	<span style="width:33%; text-align:center; float:left;">
-		<img src="img/learn.png" height="35px" /><br />
-		LEARN
-	</span>
-	<span style="width:33%; text-align:center; float:left;">
-		<img src="img/delegate.png" height="35px" /><br />
-		DISCUSS
-	</span>
-	<span style="width:33%; text-align:center; float:left;">
-		<img src="img/debate.png" height="35px" /><br />
-		DEBATE
-	</span>
+<div class = "row" style = "padding:4%;background-color:rgba(0,0,0,0.4);">
+	<div class = "col-md-8">
+		<div style="font-family:nexa;text-align:left; color:#53b5b4;">
+			<div style="box-sizing:border-box;">
+				<div style="font-weight:bold; font-size:30px;color:#53b5b4;">
+					About WAC
+				</div>
+				<div style="color:#FFFFFF;">
+				The World Affairs Conference (WAC) is Canada's oldest annual student-run current affairs conference. Annually held at Upper Canada College in Toronto, Canada, the World Affairs Conference connects over 1000 students, with a common interest in current affairs, from across North America. A dedicated team of about 100 high school students from Upper Canada College, in coordination with Branksome Hall, voluntarily contribute their time for many months leading up to the day of WAC to ensure the success and positive experience for each delegate that attends the conference. We look forward to welcoming you to the 2016 conference.
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class = "col-md-4">
+		<div class="embed-responsive embed-responsive-4by3">
+		<iframe class = "embed-responsive-item" style ="" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2884.9638959151!2d-79.40659468535445!3d43.69051397912007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b33684057775d%3A0x3dd2a9840eeeced9!2sUpper+Canada+College!5e0!3m2!1sen!2sca!4v1445541610829"></iframe>
+		</div>
+	</div>
+	<!--
+	<div class = "col-md-1">
+	</div>
+	-->
 </div>
-
+<!--
 <div style="position:absolute; top:100%; left:0px; width:100%; " id="second">
 	<div style="background-color:rgba(0,0,0,0.4); padding-left:30px; padding-right:30px; padding-bottom:70px; box-sizing:border-box;">
 		<table><tr>
@@ -111,8 +93,12 @@ include_once("navbar.php")  ?>
 			<div style="text-align:left; color:#53b5b4; font-family:nexa; font-size:70px; font-weight:bold;">
 				ABOUT WAC
 			</div>
+			
 			<div style="text-align:justify; color:#FFF; line-height:180%; font-size:15px; font-weight:300;">The World Affairs Conference (WAC) is Canada's oldest annual student-run current affairs conference. Annually held at Upper Canada College in Toronto, Canada, the World Affairs Conference connects over 800 students, with a common interest in current affairs, from across North America. A dedicated team of about 100 high school students from Upper Canada College, in coordination with Branksome Hall, voluntarily contribute their time for many months leading up to the day of WAC to ensure the success and positive experience for each delegate that attends the conference. We look forward to welcoming you to the 2016 conference. </div>
 			</br>
+			<a href="https://en.wikipedia.org/wiki/World_Affairs_Conference" target="_blank">
+				<button type="button" class="btn coloured-button">Learn more about our history</button>
+			</a>
 		</td>
 		<td style="width:10%; box-sizing:inherit;"></td>
 		<td style="width:40%; text-align:center; box-sizing:inherit;">
@@ -122,11 +108,10 @@ include_once("navbar.php")  ?>
 			</span>
 		</td></tr></table>
 	</div>
-</div>
+-->
 <?php
 include_once("footer.php")
 ?>
-</div>
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
