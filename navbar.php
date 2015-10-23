fb2f23ab8219a2072cdedd32cb4dee059a83fc0b
malsf21 <matthewang.wang@gmail.com>
2015-10-23 14:52:25 -0400

    hm apparently it aint a big deal

--- navbar.php
+++ navbar.php
@@ -10,7 +10,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt = "World Affairs Conference" height="20px"></img></a>
+      <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt = "World Affairs Conference" height="25px"></img></a>
     </div>
 
     <div class="collapse navbar-collapse" id="navbar-nav">
