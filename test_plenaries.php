4e78ea5a2153f194a764f01eac444d944b152685
malsf21 <matthewang.wang@gmail.com>
2015-10-25 11:31:13 -0400

    trying out tabs

--- test_plenaries.php
+++ test_plenaries.php
@@ -30,6 +30,29 @@ include_once("../includes/sql.php");
 <?php include_once("navbar.php") ?>
 
 <div class ="container-fluid">
+
+
+
+
+
+	<div>
+
+		<ul class="nav nav-tabs" role="tablist">
+		<li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
+		<li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
+		<li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
+		<li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
+		</ul>
+
+		<div class="tab-content">
+		<div role="tabpanel" class="tab-pane active" id="home">...</div>
+		<div role="tabpanel" class="tab-pane" id="profile">...</div>
+		<div role="tabpanel" class="tab-pane" id="messages">...</div>
+		<div role="tabpanel" class="tab-pane" id="settings">...</div>
+		</div>
+
+	</div>
+
 	<div style="text-align:center">
 	<h1>Plenaries at WAC</h1>
 	<p>Plenaries at WAC are really cool. 
