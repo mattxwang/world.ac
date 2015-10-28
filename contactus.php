cdd23b99e3685f9bc8cff3f19ad67b6ad73cbb57
malsf21 <matthewang.wang@gmail.com>
2015-10-28 19:30:45 -0400

    updated to include twitter

--- contactus.php
+++ contactus.php
@@ -55,6 +55,9 @@ include_once("../includes/sql.php");
 		<a href="https://www.facebook.com/worldaffairsconference?fref=ts">
 		<img src="img/facebook.png" class="img-responsive" style="width:30%;"></img>
 		</a>
+		<a href="https://twitter.com/WAC_UCC">
+		<img src="img/twitter.png" class="img-responsive" style="width:30%;"></img>
+		</a>
 	</div>
 </div>
 <?php
