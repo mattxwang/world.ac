bca8be66ed2983c9a310c8eee2b9134f4ed990cb
malsf21 <matthewang.wang@gmail.com>
2015-10-28 19:38:11 -0400

    restructured test files

--- test/plenaries.php
+++ test/plenaries.php
@@ -94,7 +94,7 @@ include_once("../includes/sql.php");
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et interdum arcu. Praesent auctor nunc magna, non fermentum sem fermentum sed. Sed consectetur odio eu lorem sagittis laoreet. Maecenas elementum diam eu molestie tincidunt. Ut pharetra sodales placerat. Phasellus viverra ligula dui, sed aliquet turpis auctor elementum. Phasellus sed tincidunt risus. Ut malesuada quis nibh vel sodales. Proin ut fermentum diam. Nunc eu sodales augue, commodo cursus tellus. Sed molestie consectetur arcu tincidunt ultrices. Quisque nec ultricies leo.
 				</p>
 				</div>
-
+			</div>
 		</div>
 		<div role="tabpanel" class="tab-pane" id="plenary3">
 			<h2>Plenary Heading</h2>
@@ -116,7 +116,7 @@ include_once("../includes/sql.php");
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et interdum arcu. Praesent auctor nunc magna, non fermentum sem fermentum sed. Sed consectetur odio eu lorem sagittis laoreet. Maecenas elementum diam eu molestie tincidunt. Ut pharetra sodales placerat. Phasellus viverra ligula dui, sed aliquet turpis auctor elementum. Phasellus sed tincidunt risus. Ut malesuada quis nibh vel sodales. Proin ut fermentum diam. Nunc eu sodales augue, commodo cursus tellus. Sed molestie consectetur arcu tincidunt ultrices. Quisque nec ultricies leo.
 				</p>
 				</div>
-
+			</div>
 		</div>
 		<div role="tabpanel" class="tab-pane" id="plenary4">
 			<h2>Plenary Heading</h2>
@@ -138,7 +138,7 @@ include_once("../includes/sql.php");
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et interdum arcu. Praesent auctor nunc magna, non fermentum sem fermentum sed. Sed consectetur odio eu lorem sagittis laoreet. Maecenas elementum diam eu molestie tincidunt. Ut pharetra sodales placerat. Phasellus viverra ligula dui, sed aliquet turpis auctor elementum. Phasellus sed tincidunt risus. Ut malesuada quis nibh vel sodales. Proin ut fermentum diam. Nunc eu sodales augue, commodo cursus tellus. Sed molestie consectetur arcu tincidunt ultrices. Quisque nec ultricies leo.
 				</p>
 				</div>
-
+			</div>
 		</div>
 		<div role="tabpanel" class="tab-pane" id="plenary5">
 			<h2>Plenary Heading</h2>
@@ -160,7 +160,7 @@ include_once("../includes/sql.php");
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et interdum arcu. Praesent auctor nunc magna, non fermentum sem fermentum sed. Sed consectetur odio eu lorem sagittis laoreet. Maecenas elementum diam eu molestie tincidunt. Ut pharetra sodales placerat. Phasellus viverra ligula dui, sed aliquet turpis auctor elementum. Phasellus sed tincidunt risus. Ut malesuada quis nibh vel sodales. Proin ut fermentum diam. Nunc eu sodales augue, commodo cursus tellus. Sed molestie consectetur arcu tincidunt ultrices. Quisque nec ultricies leo.
 				</p>
 				</div>
-
+			</div>
 		</div>
 		<div role="tabpanel" class="tab-pane" id="plenary6">
 			<h2>Plenary Heading</h2>
@@ -182,7 +182,7 @@ include_once("../includes/sql.php");
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et interdum arcu. Praesent auctor nunc magna, non fermentum sem fermentum sed. Sed consectetur odio eu lorem sagittis laoreet. Maecenas elementum diam eu molestie tincidunt. Ut pharetra sodales placerat. Phasellus viverra ligula dui, sed aliquet turpis auctor elementum. Phasellus sed tincidunt risus. Ut malesuada quis nibh vel sodales. Proin ut fermentum diam. Nunc eu sodales augue, commodo cursus tellus. Sed molestie consectetur arcu tincidunt ultrices. Quisque nec ultricies leo.
 				</p>
 				</div>
-
+			</div>
 		</div>
 
 	</div>
