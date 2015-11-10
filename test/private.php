1bdbea26bf8d8ff63b0f4f7199ea51c70e8c7b17
Jack Sarick <jacksarick@gmail.com>
2015-11-09 23:14:22 -0500

    removed parse example. Wasn't useful AT ALL

--- test/private.php
+++ test/private.php
@@ -10,6 +10,6 @@
 ?> 
 
 <div style="width:100%; height:100%;">
-	<h1 style="width:100%; height:50%; text-align:center; vertical-align:text-bottom;">Hello <?php echo ucfirst(htmlentities(explode($_SESSION['user']['username'], ",")), ENT_QUOTES, 'UTF-8'); ?></h1>
+	<h1 style="width:100%; height:50%; text-align:center; vertical-align:text-bottom;">Hello <?php echo ucfirst(htmlentities($_SESSION['user']['username']), ENT_QUOTES, 'UTF-8'); ?></h1>
 	<h3 style="width:100%; height:50%; text-align:center; vertical-align:top;">Shit fuckin works, yo</h3>
 </div>
