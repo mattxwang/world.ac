00d7487c13c091d44ce3b8d7d364306e04d06012
Jack Sarick <jacksarick@gmail.com>
2015-11-09 23:45:35 -0500

    More redirect

--- test/logout.php
+++ test/logout.php
@@ -7,7 +7,7 @@
 	unset($_SESSION['user']); 
 	 
 	// We redirect them to the login page 
-	header("Location: loginpage.php"); 
-	die("Redirecting to: loginpage.php");
+	header("Location: index.php"); 
+	die("Redirecting to: index.php");
 	
 ?>
