48897d815d383d2174550c512ef7845abed1af06
malsf21 <matthewang.wang@gmail.com>
2015-11-10 15:11:12 -0500

    hm

--- test/navbar.php
+++ test/navbar.php
@@ -76,9 +76,7 @@
 								</div>
 								<div class = "col-md-4">
 									<button class="btn btn-small btn-success btn-block" id="submitbutton" type="submit" value="Login">Sign in</button>
-									<div style = "text-align:right;padding-right:20px;">
-										<a href="registerpage.php" style = "text-decoration: underline; color:black;font-size:8px;">Don't have an account?</a>
-									</div>
+									<a href="register_page.php" style = "text-decoration: underline; color:black;font-size:8px;">Don't have an account?</a>
 								</div>
 							</form>
 							</div>
