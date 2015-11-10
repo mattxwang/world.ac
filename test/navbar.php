44714299dfcebb43425c96c97112588ad6c7b1ca
malsf21 <matthewang.wang@gmail.com>
2015-11-10 15:04:29 -0500

    fine

--- test/navbar.php
+++ test/navbar.php
@@ -75,7 +75,7 @@
 									<input type="password" id="password" name="password" class="form-control" placeholder="Password" required="">
 								</div>
 								<div class = "col-md-4">
-									<button class="btn btn-small btn-success" id="submitbutton" type="submit" value="Login">Sign in</button>
+									<button class="btn btn-small btn-success btn-block" id="submitbutton" type="submit" value="Login">Sign in</button>
 								</div>
 							</form>
 							</div>
