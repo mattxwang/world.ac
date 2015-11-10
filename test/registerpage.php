41ca72a064b83f7636b45d587c754dec7143fabc
Matthew Wang <matthewang.wang@gmail.com>
2015-11-09 23:13:54 -0500

    stylistic changes

--- test/registerpage.php
+++ test/registerpage.php
@@ -17,7 +17,7 @@
 <body style="zoom: 1;">
 		<div class="container">
 			<div style = "text-align:left;">
-				<h2>Please Register</h2>
+				<h2>WAC 2016 Account Registration</h2>
 			</div>
 			<div class ="row">
 				<div class = "col-md-6">
@@ -53,7 +53,7 @@
 					</form>
 				</div>
 				<div class = "col-md-6" style = "text-align: justify;">
-					<h3 class="form-signin-heading">Why register for a WAC 2016 account?</h3>
+					<h3>Why register for a WAC 2016 account?</h3>
 					<p>
 					The WAC 2016 account does a few things for you! First of all, it allows us to check whether or not you've been correctly registered for WAC!
 					</br>
@@ -63,33 +63,6 @@
 					</p>
 				</div>
 			</div>
-				<!--
-				<form class="form-signin" action="register.php" method="post">
-					<h2 class="form-signin-heading">Please Register</h2>
-
-					<input style="width:50%;" type="text" id="firstname" class="form-control" placeholder="First Name" name="firstname" required="" autofocus="">
-					<input style="width:50%;" type="text" id="lastname" class="form-control" placeholder="Last Name" name="lastname" required="" autofocus="">
-
-					<input type="email" id="email" class="form-control" placeholder="user@email.com" name="email" required="" autofocus="">
-
-					<input type="password" id="password" name="password" class="form-control" placeholder="Password" required="">
-					<input type="password" id="password2" name="password2" class="form-control" placeholder="Re-Type Password" required="">
-
-					<button class="btn btn-lg btn-primary btn-block" type="submit" value="Login">Register</button>
-				</form>
-		
-			</div>
-			<div style = "text-align: justify;">
-				<h3 class="form-signin-heading">Why register for a WAC 2016 account?</h3>
-				<p>
-				The WAC 2016 account does a few things for you! First of all, it allows us to check whether or not you've been correctly registered for WAC!
-				</br>
-				It also lets you check up on your plenaries, whether or not your payment has been accepted, and to make sure that all of your delegate information is correct.
-				</br>
-				You can only sign up for a WAC 2016 account if you have already been registered as a delegate. If you're getting a registration error, feel free to <a href ="contact_us.php">contact us</a>.
-				</p>
-			</div>
-			-->
 			<?php
 			include_once("footer.php")
 			?>
