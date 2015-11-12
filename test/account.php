ed05a47c224e7db1721769e66ef045c2ab265013
malsf21 <matthewang.wang@gmail.com>
2015-11-12 12:36:39 -0500

    added dismiss buttons

--- test/account.php
+++ test/account.php
@@ -82,17 +82,37 @@
 							</div>
 						</div>
 
-						<div class="alert alert-dismissible alert-warning" role="alert">You have not been successfuly registered.</div>
-						<div class="alert alert-dismissible alert-success" role="alert">You have been successfuly registered.</div>
+						<div class="alert alert-dismissible alert-warning" role="alert">You have not been successfuly registered.
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						</div>
 
-						<div class="alert alert-dismissible alert-info" role="alert">Plenaries haven't been assigned yet. Check back soon.</div>
-						<div class="alert alert-dismissible alert-success" role="alert">Plenaries have been assigned. Check them out below.</div>
+						<div class="alert alert-dismissible alert-success" role="alert">You have been successfuly registered.
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						</div>
 
-						<div class="alert alert-dismissible alert-warning" role="alert">Your email hasn't been confirmed yet.</div>
-						<div class="alert alert-dismissible alert-success" role="alert">Your email has been confirmed.</div>
+						<div class="alert alert-dismissible alert-info" role="alert">Plenaries haven't been assigned yet. Check back soon.
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						</div>
 
-						<div class="alert alert-dismissible alert-danger" role="alert">We haven't received your payment yet.</div>
-						<div class="alert alert-dismissible alert-success" role="alert">We've received your payment.</div>
+						<div class="alert alert-dismissible alert-success" role="alert">Plenaries have been assigned. Check them out below.
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						</div>
+
+						<div class="alert alert-dismissible alert-warning" role="alert">Your email hasn't been confirmed yet.
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						</div>
+
+						<div class="alert alert-dismissible alert-success" role="alert">Your email has been confirmed.
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						</div>
+
+						<div class="alert alert-dismissible alert-danger" role="alert">We haven't received your payment yet.
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						</div>
+
+						<div class="alert alert-dismissible alert-success" role="alert">We've received your payment.
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						</div>
 
 					</div>
 					<div id = "info">
