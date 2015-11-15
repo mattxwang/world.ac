47e3e003b78f2a923a4fc46ef479d60f93477f25
malsf21 <matthewang.wang@gmail.com>
2015-11-14 21:05:58 -0500

    new branksome color

--- test/about_us.php
+++ test/about_us.php
@@ -32,7 +32,7 @@ Head of Communications and Head of Security: Justin Lu-->
 	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
 	<style>
 	.bg-bh{
-		background-color:#1A6311;
+		background-color:#C3DD47;
 	}
 	.bg-ucc{
 		background-color:#5E7794;
