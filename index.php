6498629a431864521c4c363f8ac314d7ba10395b
Matthew Wang <matthewang.wang@gmail.com>
2015-11-20 00:12:40 -0500

    implements test index into main

--- index.php
+++ index.php
@@ -21,55 +21,63 @@ include_once("../includes/sql.php");
 	<link href="css/bootstrap/bootstrap-theme.css" rel="stylesheet">
 	<link rel="stylesheet" href="css/base.css" />
 	<script src="js/countdown.js"></script>
-	<script type="text/javascript" src="js/easing.js"></script>
+	<style>
+	body{
+		background-image:url("img/earth.png"),url("img/stars.png");
+		background-repeat:no-repeat;
+	}
+	</style>
 </head>
 
 <body>
-<?php include_once("tracking.php")  ?>
+	<?php include_once("navbar.php")  ?>
+	<div style="line-height:80%; padding:4%; position:relative">
+		<div style="font-size:5vw;">
+			WELCOME TO THE 2016 <div style="font-weight:bold; padding-top: 5vw;">WORLD AFFAIRS CONFERENCE.</div>
+		</div>
+		<div style="padding:0 10px 0 10px; padding-top:5vw; font-size:3vw; text-align:right"> The Conference Starts In: <div id="timeLeft" style="padding-top:3vw;">February 1st 2016</div></div>
+		<script type="text/javascript">
+			var deadLine = "2/1/2016";
+			window.setInterval(function(){
+				// console.log(getTimeRemaining(deadLine));
+				$("#timeLeft").html(getTimeRemaining(deadLine));
+			}, 1000);
+		</script>
 
-<div id="bg_stars"><div></div></div><div id="bg_earth"></div>
-
-<?php
-include_once("navbar.php")  ?>
-
-<div style="font-family:Nexa; line-height:80%; color:#FFF; padding:4%; padding-top:5%; position:relative">
-	<div style="font-size:5vw;">
-		WELCOME TO THE 2016 <div style="font-weight:bold; padding-top: 5vw;">WORLD AFFAIRS CONFERENCE.</div>
 	</div>
-	<div style="padding:0 10px 0 10px; padding-top:5vw; font-size:3vw; text-align:right"> The Conference Starts In: <div id="timeLeft" style="padding-top:3vw;">February 1st 2016</div></div>
-	<script type="text/javascript">
-		var deadLine = "2/1/2016";
-		window.setInterval(function(){
-			// console.log(getTimeRemaining(deadLine));
-			$("#timeLeft").html(getTimeRemaining(deadLine));
-		}, 1000);
-	</script>
+	<!--
+	<div style="line-height:80%; padding:4%; position:relative" class = "visible-xs">
+		<h1>WELCOME TO THE 2016 </br><b>WORLD AFFAIRS CONFERENCE.</b><h1>
+		<div style="text-align:right"> <h2>The Conference Starts In:</h2><div id="timeLeft"><h3></h3></div></div>
+		<script type="text/javascript">
+			var deadLine = "2/1/2016";
+			window.setInterval(function(){
+				// console.log(getTimeRemaining(deadLine));
+				$("#timeLeft").html(getTimeRemaining(deadLine));
+			}, 1000);
+		</script>
 
-</div>
-<div class = "row" style = "padding:4%;background-color:rgba(0,0,0,0.4);">
-	<div class = "col-md-8">
-		<div style="font-family:nexa;text-align:left; color:#53b5b4;">
-			<div style="box-sizing:border-box;">
-				<div style="font-weight:bold; font-size:30px;color:#53b5b4;">
-					About WAC
-				</div>
-				<div class = "text-justify" style="color:#FFFFFF;">
-				The World Affairs Conference (WAC) is Canada's oldest annual student-run current affairs conference. Annually held at Upper Canada College in Toronto, Canada, the World Affairs Conference connects over 1000 students, with a common interest in current affairs, from across North America. A dedicated team of about 100 high school students from Upper Canada College, in coordination with Branksome Hall, voluntarily contribute their time for many months leading up to the day of WAC to ensure the success and positive experience for each delegate that attends the conference. We look forward to welcoming you to the 2016 conference. 
-				</div>
+	</div>
+	-->
+	<div class = "row" style = "padding:4%;background-color:rgba(0,0,0,0.4);">
+		<div class = "col-md-8">
+					<h2 style = "color:#53b5b4;"><b>About WAC</b></h2>
+					<p>
+					The World Affairs Conference (WAC) is Canada's oldest annual student-run current affairs conference. Annually held at Upper Canada College in Toronto, Canada, the World Affairs Conference connects over 1000 students, with a common interest in current affairs, from across North America. A dedicated team of about 100 high school students from Upper Canada College, in coordination with Branksome Hall, voluntarily contribute their time for many months leading up to the day of WAC to ensure the success and positive experience for each delegate that attends the conference. We look forward to welcoming you to the 2016 conference. 
+					</p>
+		</div>
+		<div class = "col-md-4">
+			<div class="embed-responsive embed-responsive-4by3">
+			<iframe class = "embed-responsive-item" style ="" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2884.9638959151!2d-79.40659468535445!3d43.69051397912007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b33684057775d%3A0x3dd2a9840eeeced9!2sUpper+Canada+College!5e0!3m2!1sen!2sca!4v1445541610829"></iframe>
 			</div>
 		</div>
 	</div>
-	<div class = "col-md-4">
-		<div class="embed-responsive embed-responsive-4by3">
-		<iframe class = "embed-responsive-item" style ="" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2884.9638959151!2d-79.40659468535445!3d43.69051397912007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b33684057775d%3A0x3dd2a9840eeeced9!2sUpper+Canada+College!5e0!3m2!1sen!2sca!4v1445541610829"></iframe>
-		</div>
-	</div>
-</div>
-<?php
-include_once("footer.php")
-?>
+	<?php
+	include_once("footer.php")
+	?>
 
-<script src="js/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
 </body>
 </html>
