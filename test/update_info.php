386158ed07f984474735a97e37b704a9c8b3208b
Jack Sarick <jacksarick@gmail.com>
2015-11-24 17:43:48 -0500

    Almost full roll forward, NO NULL CHECKING. UNFIT FOR USE

--- test/update_info.php
+++ test/update_info.php
@@ -17,37 +17,37 @@
 			':notes'    => $_POST['notes']
 		); 
 
-		// // Make query
-		// $query = " 
-		// REPLACE INTO info ( 
-		// 	username, 
-		// 	plen_1, 
-		// 	plen_2,
-		// 	plen_3, 
-		// 	school,
-		// 	notes
-		// ) VALUES ( 
-		// 	:username, 
-		// 	:plen_1, 
-		// 	:plen_2,
-		// 	:plen_3, 
-		// 	:school,
-		// 	:notes 
-		// );";
+		// Make query
+		$query = " 
+		REPLACE INTO info ( 
+			username, 
+			plen_1, 
+			plen_2,
+			plen_3, 
+			school,
+			notes
+		) VALUES ( 
+			:username, 
+			:plen_1, 
+			:plen_2,
+			:plen_3, 
+			:school,
+			:notes 
+		);";
 
-		// // Execute said query
-		// try { 
-		// 	// These two statements run the query against your database table. 
-		// 	$stmt = $db->prepare($query); 
-		// 	$stmt->execute();
+		// Execute said query
+		try { 
+			// These two statements run the query against your database table. 
+			$stmt = $db->prepare($query); 
+			$stmt->execute();
 		
-		// 	header("Location: account.php"); 		 
-		// }
+			header("Location: account.php"); 		 
+		}
 		
-		// catch(PDOException $ex) 
-		// { 
-		// 	die("Failed"); 
-			// header("Location: account.php");
-		// }
+		catch(PDOException $ex) 
+		{ 
+			die("Failed"); 
+			header("Location: account.php");
+		}
 	}
 ?>
