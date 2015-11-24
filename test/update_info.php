a9bbdecb63ea1193f8a2f2b685f8f31f4e6c10c2
Jack Sarick <jacksarick@gmail.com>
2015-11-24 17:33:02 -0500

    Finished update info 0.1

--- test/update_info.php
+++ test/update_info.php
@@ -1,16 +1,25 @@
 <?php 
 	require("common.php"); 
+
+	// Make sure they did stuff
 	if(!empty($_POST)) { 
 		if(empty($_POST['firstname']) || empty($_POST['lastname']) || /*KEEP GOING*/) { 
 			die("You missed a field"); 
 		}
 
+		// Get all useful info
 		$query_params = array( 
-			':username' => $_POST['firstname'] . "," . $_POST['lastname'] 
+			':username' => $_POST['firstname'] . "," . $_POST['lastname'],
+			':plen_1'   => NULL,
+			':plen_2'   => NULL,
+			':plen_3'   => NULL,
+			':school'   => $_POST['school'],
+			':notes'    => $_POST['notes']
 		); 
 
+		// Make query
 		$query = " 
-		INSERT INTO info ( 
+		REPLACE INTO info ( 
 			username, 
 			plen_1, 
 			plen_2,
@@ -24,6 +33,20 @@
 			:plen_3, 
 			:school,
 			:notes 
-		);
-		"; 
+		);";
+
+		// Execute said query
+		try { 
+			// These two statements run the query against your database table. 
+			$stmt = $db->prepare($query); 
+			$stmt->execute();
+		
+			header("Location: account.php"); 		 
+		}
+		
+		catch(PDOException $ex) 
+		{ 
+			die("Failed"); 
+		}
+	}
 ?>
