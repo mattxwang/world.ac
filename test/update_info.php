93a626fd03757f18bfcbbf77b1daedf9152a7878
Jack Sarick <jacksarick@gmail.com>
2015-11-24 17:19:52 -0500

    Started update_info

--- test/update_info.php
+++ test/update_info.php
@@ -1,64 +1,29 @@
 <?php 
 	require("common.php"); 
-	 
-	$submitted_email = ''; 
-	$correction = 'none';
-	 
-	if(!empty($_POST)) 
-	{ 
-		$query = " 
-			SELECT * 
-			FROM users 
-			WHERE 
-				email = :email"; 
-		 
+	if(!empty($_POST)) { 
+		if(empty($_POST['firstname']) || empty($_POST['lastname']) || /*KEEP GOING*/) { 
+			die("You missed a field"); 
+		}
+
 		$query_params = array( 
-			':email' => $_POST['email'] 
+			':username' => $_POST['firstname'] . "," . $_POST['lastname'] 
 		); 
-		 
-		try 
-		{ 
-			$stmt = $db->prepare($query); 
-			$result = $stmt->execute($query_params); 
-		} 
-		catch(PDOException $ex) 
-		{ 
-			die("Failed to run query: " . $ex->getMessage()); 
-		} 
-		 
-		$login_ok = false; 
-		 
-		$row = $stmt->fetch(); 
-		if($row) 
-		{ 
-			$check_password = hash('sha256', $_POST['password'] . $row['salt']); 
-			for($round = 0; $round < 65536; $round++) 
-			{ 
-				$check_password = hash('sha256', $check_password . $row['salt']); 
-			} 
-			 
-			if($check_password === $row['password']) 
-			{ 
-				$login_ok = true; 
-			} 
-		} 
-		 
-		if($login_ok) 
-		{ 
-			unset($row['salt']); 
-			unset($row['password']); 
-			 
-			$_SESSION['user'] = $row; 
-			 
-			header("Location: account.php"); 
-			die("Redirecting to: account.php"); 
-		} 
-		else 
-		{ 
-			$correction = 'block'; 
-			 
-			$submitted_email = htmlentities($_POST['email'], ENT_QUOTES, 'UTF-8'); 
-		} 
-	} 
-	 
+
+		$query = " 
+		INSERT INTO info ( 
+			username, 
+			plen_1, 
+			plen_2,
+			plen_3, 
+			school,
+			notes
+		) VALUES ( 
+			:username, 
+			:plen_1, 
+			:plen_2,
+			:plen_3, 
+			:school,
+			:notes 
+		);
+		"; 
 ?>
