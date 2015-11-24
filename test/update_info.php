66b128c2b972ec438544a5ccc82dbb82183eb9e2
Jack Sarick <jacksarick@gmail.com>
2015-11-24 17:39:12 -0500

    update_info testing

--- test/update_info.php
+++ test/update_info.php
@@ -3,50 +3,50 @@
 
 	// Make sure they did stuff
 	if(!empty($_POST)) { 
-		if(empty($_POST['firstname']) || empty($_POST['lastname']) || /*KEEP GOING*/) { 
-			die("You missed a field"); 
-		}
+		// if(empty($_POST['firstname']) || empty($_POST['lastname']) || /*KEEP GOING*/) { 
+		// 	die("You missed a field"); 
+		// }
 
-		// Get all useful info
-		$query_params = array( 
-			':username' => $_POST['firstname'] . "," . $_POST['lastname'],
-			':plen_1'   => NULL,
-			':plen_2'   => NULL,
-			':plen_3'   => NULL,
-			':school'   => $_POST['school'],
-			':notes'    => $_POST['notes']
-		); 
+		// // Get all useful info
+		// $query_params = array( 
+		// 	':username' => $_POST['firstname'] . "," . $_POST['lastname'],
+		// 	':plen_1'   => NULL,
+		// 	':plen_2'   => NULL,
+		// 	':plen_3'   => NULL,
+		// 	':school'   => $_POST['school'],
+		// 	':notes'    => $_POST['notes']
+		// ); 
 
-		// Make query
-		$query = " 
-		REPLACE INTO info ( 
-			username, 
-			plen_1, 
-			plen_2,
-			plen_3, 
-			school,
-			notes
-		) VALUES ( 
-			:username, 
-			:plen_1, 
-			:plen_2,
-			:plen_3, 
-			:school,
-			:notes 
-		);";
+		// // Make query
+		// $query = " 
+		// REPLACE INTO info ( 
+		// 	username, 
+		// 	plen_1, 
+		// 	plen_2,
+		// 	plen_3, 
+		// 	school,
+		// 	notes
+		// ) VALUES ( 
+		// 	:username, 
+		// 	:plen_1, 
+		// 	:plen_2,
+		// 	:plen_3, 
+		// 	:school,
+		// 	:notes 
+		// );";
 
-		// Execute said query
-		try { 
-			// These two statements run the query against your database table. 
-			$stmt = $db->prepare($query); 
-			$stmt->execute();
+		// // Execute said query
+		// try { 
+		// 	// These two statements run the query against your database table. 
+		// 	$stmt = $db->prepare($query); 
+		// 	$stmt->execute();
 		
-			header("Location: account.php"); 		 
-		}
+		// 	header("Location: account.php"); 		 
+		// }
 		
-		catch(PDOException $ex) 
-		{ 
-			die("Failed"); 
-		}
+		// catch(PDOException $ex) 
+		// { 
+		// 	die("Failed"); 
+		// }
 	}
 ?>
