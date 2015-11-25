1785d87438b696c756112ab535ab58613293f9d9
Jack Sarick <jacksarick@gmail.com>
2015-11-25 17:41:17 -0500

    update_info: testing POST

--- test/update_info.php
+++ test/update_info.php
@@ -1,59 +1,61 @@
 <?php 
 	require("common.php"); 
 
-	// Make sure they did stuff
-	if(!empty($_POST)) { 
-		// if(empty($_POST['firstname']) || empty($_POST['lastname']) || /*KEEP GOING*/) { 
-		// 	die("You missed a field"); 
-		// }
+	var_dump($_POST);
 
-		// Get all useful info
-		// $query_params = array( 
-		// 	':username' => $_POST['firstname'] . "," . $_POST['lastname'],
-		// 	':plen_1'   => NULL,
-		// 	':plen_2'   => NULL,
-		// 	':plen_3'   => NULL,
-		// 	':school'   => $_POST['school'],
-		// 	':notes'    => $_POST['notes']
-		// ); 
+	// // Make sure they did stuff
+	// // if(!empty($_POST)) { 
+	// 	// if(empty($_POST['firstname']) || empty($_POST['lastname']) || /*KEEP GOING*/) { 
+	// 	// 	die("You missed a field"); 
+	// 	// }
 
-		// $query_params = array( 
-		// 	':username' => $_POST['firstname'] . "," . $_POST['lastname'] 
-		// ); 
+	// 	// Get all useful info
+	// 	// $query_params = array( 
+	// 	// 	':username' => $_POST['firstname'] . "," . $_POST['lastname'],
+	// 	// 	':plen_1'   => NULL,
+	// 	// 	':plen_2'   => NULL,
+	// 	// 	':plen_3'   => NULL,
+	// 	// 	':school'   => $_POST['school'],
+	// 	// 	':notes'    => $_POST['notes']
+	// 	// ); 
 
-		$username = $_POST['firstname'] . "," . $_POST['lastname'];
+	// 	$query_params = array( 
+	// 		':username' => $_POST['firstname'] . "," . $_POST['lastname'] 
+	// 	); 
 
-		// Make query
-		$query = " 
-		REPLACE INTO info ( 
-			username, 
-			plen_1, 
-			plen_2,
-			plen_3, 
-			school,
-			notes
-		) VALUES (
-			" . $username . ",
-			NUll, 
-			NUll, 
-			NUll, "
-			. $_POST['school'] . ",",
-			. $_POST['notes'] .
-		");";
+	// 	// $username = $_POST['firstname'] . "," . $_POST['lastname'];
 
-		// Execute said query
-		try { 
-			// These two statements run the query against your database table. 
-			$stmt = $db->prepare($query); 
-			$stmt->execute();
+	// 	// Make query
+	// 	$query = " 
+	// 	REPLACE INTO info ( 
+	// 		username, 
+	// 		plen_1, 
+	// 		plen_2,
+	// 		plen_3, 
+	// 		school,
+	// 		notes
+	// 	) VALUES (
+	// 		:username,
+	// 		NUll, 
+	// 		NUll, 
+	// 		NUll, "
+	// 		. $_POST['school'] . ",",
+	// 		. $_POST['notes'] .
+	// 	");";
+
+	// 	// Execute said query
+	// 	try { 
+	// 		// These two statements run the query against your database table. 
+	// 		$stmt = $db->prepare($query); 
+	// 		$stmt->execute();
 		
-			header("Location: account.php"); 		 
-		}
+	// 		header("Location: account.php"); 		 
+	// 	}
 		
-		catch(PDOException $ex) 
-		{ 
-			die("Failed to run query: " . $ex->getMessage()); 
-			header("Location: account.php");
-		}
-	}
+	// 	catch(PDOException $ex) 
+	// 	{ 
+	// 		die("Failed to run query: " . $ex->getMessage()); 
+	// 		header("Location: account.php");
+	// 	}
+	// }
 ?>
