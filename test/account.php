06aceb56a45a4f461f31a99d87bdab6725ddedd2
Jack Sarick <jacksarick@gmail.com>
2015-11-25 17:43:23 -0500

    Touch up of account

--- test/account.php
+++ test/account.php
@@ -113,10 +113,10 @@
 											
 
 								
-									</div>
-									<div class="modal-footer">
-										<button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
-										<button type="submit" class="btn btn-primary" id="submitbutton" value="Login" >Save Changes</button>
+											</div>
+											<div class="modal-footer">
+												<button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+												<button type="submit" class="btn btn-primary" id="submitbutton" value="Login" >Save Changes</button>
 										</form>
 									</div>
 								</div>
