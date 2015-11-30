dce1edbd53dcaf8d838d1611827d3edf1a9a0c4a
malsf21 <matthewang.wang@gmail.com>
2015-11-29 19:31:39 -0500

    comment out pro bar

--- test/account.php
+++ test/account.php
@@ -196,11 +196,13 @@
 					</div>
 
 					<div id = "notif">
+						<!--
 						<div class="progress" style = "height:36px;">
 							<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:50%;">
 								You've completed 50% of the registration process.
 							</div>
 						</div>
+						-->
 
 						<!-- NOTIFICATIONS -->
 						<div class=<?php echo $reg_class;?> role="alert"><?php echo $reg_text; ?>
