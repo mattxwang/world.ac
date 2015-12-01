3907dd85f93e19bce9b7bdd073dc6a649389b3b6
Jack Sarick <jacksarick@gmail.com>
2015-11-30 21:37:15 -0500

    Nope. not the (only) problem

--- test/update_info.php
+++ test/update_info.php
@@ -1,95 +1,96 @@
 <?php 
 	require("common.php"); 
 
-	// V1
-	var_dump($_POST);
+	//// V1
+	// var_dump($_POST);
 
-	// Make sure they did stuff
-	if(!empty($_POST)) { 
-		// if(empty($_POST['firstname']) || empty($_POST['lastname']) || /*KEEP GOING*/) { 
-		// 	die("You missed a field"); 
-		// }
+	// // Make sure they did stuff
+	// // if(!empty($_POST)) { 
+	// 	// if(empty($_POST['firstname']) || empty($_POST['lastname']) || /*KEEP GOING*/) { 
+	// 	// 	die("You missed a field"); 
+	// 	// }
 
-		// Get all useful info
-		// $query_params = array( 
-		// 	':username' => $_POST['firstname'] . "," . $_POST['lastname'],
-		// 	':plen_1'   => NULL,
-		// 	':plen_2'   => NULL,
-		// 	':plen_3'   => NULL,
-		// 	':school'   => $_POST['school'],
-		// 	':notes'    => $_POST['notes']
-		// ); 
+	// 	// Get all useful info
+	// 	// $query_params = array( 
+	// 	// 	':username' => $_POST['firstname'] . "," . $_POST['lastname'],
+	// 	// 	':plen_1'   => NULL,
+	// 	// 	':plen_2'   => NULL,
+	// 	// 	':plen_3'   => NULL,
+	// 	// 	':school'   => $_POST['school'],
+	// 	// 	':notes'    => $_POST['notes']
+	// 	// ); 
 
-		$query_params = array( 
-			':username' => $_POST['firstname'] . "," . $_POST['lastname'] 
-		); 
+	// 	$query_params = array( 
+	// 		':username' => $_POST['firstname'] . "," . $_POST['lastname'] 
+	// 	); 
 
-		// $username = $_POST['firstname'] . "," . $_POST['lastname'];
+	// 	// $username = $_POST['firstname'] . "," . $_POST['lastname'];
 
-		// Make query
-		$query = " 
-		REPLACE INTO info ( 
-			username, 
-			plen_1, 
-			plen_2,
-			plen_3, 
-			school,
-			notes
-		) VALUES (
-			:username,
-			'none', 
-			'none', 
-			'none', "
-			. $_POST['school'] . ",",
-			. $_POST['notes'] .
-		");";
+	// 	// Make query
+	// 	$query = " 
+	// 	REPLACE INTO info ( 
+	// 		username, 
+	// 		plen_1, 
+	// 		plen_2,
+	// 		plen_3, 
+	// 		school,
+	// 		notes
+	// 	) VALUES (
+	// 		:username,
+	// 		'none', 
+	// 		'none', 
+	// 		'none', "
+	// 		. $_POST['school'] . ",",
+	// 		. $_POST['notes'] .
+	// 	");";
 
-		// Execute said query
-		try { 
-			// These two statements run the query against your database table. 
-			$stmt = $db->prepare($query); 
-			$stmt->execute();
+	// 	// Execute said query
+	// 	try { 
+	// 		// These two statements run the query against your database table. 
+	// 		$stmt = $db->prepare($query); 
+	// 		$stmt->execute();
 		
-			header("Location: account.php"); 		 
-		}
+	// 		header("Location: account.php"); 		 
+	// 	}
 		
-		catch(PDOException $ex) 
-		{ 
-			die("Failed to run query: " . $ex->getMessage()); 
-			header("Location: account.php");
-		}
+	// 	catch(PDOException $ex) 
+	// 	{ 
+	// 		die("Failed to run query: " . $ex->getMessage()); 
+	// 		header("Location: account.php");
+	// 	}
+	// }
+
+	$username = $_POST['firstname'] . "," . $_POST['lastname'];
+
+	// THE TESTING AREA
+	$query = " 
+	REPLACE INTO info ( 
+		username, 
+		plen_1, 
+		plen_2,
+		plen_3, 
+		school,
+		notes
+	) VALUES (
+		" . $username . ",
+		'none', 
+		'none', 
+		'none',
+		'UCC',
+		'LOLOLOL'
+	);";
+	// Execute said query
+	try { 
+		// These two statements run the query against your database table. 
+		$stmt = $db->prepare($query); 
+		$stmt->execute();
+	
+		header("Location: account.php"); 		 
 	}
-
-
-	// THE NULL TEST THAT SOLVED THE PROBS
-	// $query = " 
-	// REPLACE INTO info ( 
-	// 	username, 
-	// 	plen_1, 
-	// 	plen_2,
-	// 	plen_3, 
-	// 	school,
-	// 	notes
-	// ) VALUES (
-	// 	'Jack,Sarick',
-	// 	'none', 
-	// 	'none', 
-	// 	'none',
-	// 	'UCC',
-	// 	'LOLOLOL'
-	// );";
-	// // Execute said query
-	// try { 
-	// 	// These two statements run the query against your database table. 
-	// 	$stmt = $db->prepare($query); 
-	// 	$stmt->execute();
 	
-	// 	header("Location: account.php"); 		 
-	// }
-	
-	// catch(PDOException $ex) 
-	// { 
-	// 	die("Failed to run query: " . $ex->getMessage()); 
-	// 	header("Location: account.php");
-	// }
+	catch(PDOException $ex) 
+	{ 
+		die("Failed to run query: " . $ex->getMessage()); 
+		header("Location: account.php");
+	}
 ?>
