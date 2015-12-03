1dde21fca38f404a57c9df085abcf3cebebe25c6
Jack Sarick <jacksarick@gmail.com>
2015-12-02 21:42:24 -0500

    updated update+_info to new framework

--- test/update_info.php
+++ test/update_info.php
@@ -1,51 +1,40 @@
 <?php 
 	require("common.php"); 
 
-	//// V1
-	// var_dump($_POST);
-
 	// Make sure they did stuff
-	// if(!empty($_POST)) { 
-		// if(empty($_POST['firstname']) || empty($_POST['lastname']) || /*KEEP GOING*/) { 
-		// 	die("You missed a field"); 
-		// }
+	if(!empty($_POST)) { 
+		if(empty($_POST['firstname']) || empty($_POST['lastname']) || /*KEEP GOING*/) { 
+			die("You missed a field"); 
+		}
 
-		// Get all useful info
-		// $query_params = array( 
-		// 	':username' => $_POST['firstname'] . "," . $_POST['lastname'],
-		// 	':plen_1'   => NULL,
-		// 	':plen_2'   => NULL,
-		// 	':plen_3'   => NULL,
-		// 	':school'   => $_POST['school'],
-		// 	':notes'    => $_POST['notes']
-		// ); 
-
-		// $query_params = array( 
-		// 	':username' => $_POST['firstname'] . "%$%" . $_POST['lastname'],
-		// 	':email' => $_SESSION['user']['username']
-		// ); 
-
-		// $username = $_POST['firstname'] . "," . $_POST['lastname'];
+		//Prep variables
+		$email = $_SESSION['user']['email'];
+		$f_name = $_POST['first_name'];
+		$l_name = $_POST['last_name'];
+		$school = $_POST['school'];
+		$notes = $_POST['notes'];
 
 		// Make query
 		$query = " 
 		REPLACE INTO info (
-			email,
 			username,
+			first_name,
+			last_name,
 			plen_1, 
 			plen_2,
 			plen_3, 
 			school,
 			notes
 		) VALUES (
-			" . $_SESSION['user']['email'] . ",
-			" . $_SESSION['user']['username'] . ",
+			'$email',
+			'$f_name',
+			'$l_name',
 			'none', 
 			'none', 
-			'none', "
-			. $_POST['school'] . ",",
-			. $_POST['notes'] .
-		");";
+			'none',
+			'$school',
+			'$notes'
+		);";
 
 		// Execute said query
 		try { 
@@ -61,41 +50,5 @@
 			die("Failed to run query: " . $ex->getMessage()); 
 			header("Location: account.php");
 		}
-	// }
-
-	// $username = $_POST['firstname'] . "$$$" . $_POST['lastname'];
-
-	// // THE TESTING AREA
-	// $query = " 
-	// REPLACE INTO info (
-	// 	email,
-	// 	username, 
-	// 	plen_1, 
-	// 	plen_2,
-	// 	plen_3, 
-	// 	school,
-	// 	notes
-	// ) VALUES (
-	// 	:email,
-	// 	" . $username . ",
-	// 	'none', 
-	// 	'none', 
-	// 	'none',
-	// 	'UCC',
-	// 	'LOLOLOL'
-	// );";
-	// // Execute said query
-	// try { 
-	// 	// These two statements run the query against your database table. 
-	// 	$stmt = $db->prepare($query); 
-	// 	$stmt->execute();
-	
-	// 	header("Location: account.php"); 		 
-	// }
-	
-	// catch(PDOException $ex) 
-	// { 
-	// 	die("Failed to run query: " . $ex->getMessage()); 
-	// 	header("Location: account.php");
-	// }
+	}
 ?>
