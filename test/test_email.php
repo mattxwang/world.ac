e896d04d9927b39a31fcaa366a56b83b9d9d0ed2
Jack Sarick <jacksarick@gmail.com>
2015-12-02 22:20:40 -0500

    Nope

--- test/test_email.php
+++ test/test_email.php
@@ -1,18 +1,12 @@
 <?php
 // the message
-$message = "First line of text\nSecond line of text";
+$msg = "First line of text\nSecond line of text";
 
 // use wordwrap() if lines are longer than 70 characters
-$message = wordwrap($message,70);
-
-$to = "jack.sarick@ucc.on.ca"
-
-$subject = "This is a test"
-
-$headers = 'From: donotrespond@world.ac';
+$msg = wordwrap($msg,70);
 
 // send email
-mail($to, $subject, $message, $headers);
+mail("jack.sarick@ucc.on.ca","My subject",$msg);
 
 echo "It worked!";
 ?>
