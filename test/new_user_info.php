c5e538886c4e9cfb34045627284411bc12c7a7f1
Jack Sarick <jacksarick@gmail.com>
2015-12-02 20:20:22 -0500

    Defucked it

--- test/new_user_info.php
+++ test/new_user_info.php
@@ -1,39 +1,38 @@
 <?php 
-	require("common.php"); 
-		if(empty($_POST['first_name']) ||empty($_POST['last_name'])||empty($_POST['school'])||empty($_POST['notes'])) { 
-			die("You missed a field"); 
-		} 
-		$query = " 
-		INSERT INTO info (
-			email,
-			first_name,
-			last_name,
-			plen_1, 
-			plen_2,
-			plen_3, 
-			school,
-			notes
-		) VALUES (
-			" . $_SESSION['user']['email'] . ","
-			. $_POST['first_name'] . ""
-			. $_POST['last_name'] . "
-			'none', 
-			'none', 
-			'none', "
-			. $_POST['school'] . ",",
-			. $_POST['notes'] .
-		");";
+	if(empty($_POST['first_name']) ||empty($_POST['last_name'])||empty($_POST['school']))) { 
+		die("You missed a field"); 
+	}
+	$query = " 
+	INSERT INTO info (
+		email,
+		first_name,
+		last_name,
+		plen_1, 
+		plen_2,
+		plen_3, 
+		school,
+		notes
+	) VALUES (
+		" . $_SESSION['user']['email'] . ","
+		. $_POST['first_name'] . ""
+		. $_POST['last_name'] . "
+		'none', 
+		'none', 
+		'none', "
+		. $_POST['school'] . ",",
+		. $_POST['notes'] .
+	");";
 
-		try { 
-			$stmt = $db->prepare($query); 
-			$stmt->execute();
-		
-			header("Location: account.php"); 		 
-		}
-		
-		catch(PDOException $ex) 
-		{ 
-			die("Failed to run query: " . $ex->getMessage()); 
-			header("Location: account.php");
-		}
+	try { 
+		$stmt = $db->prepare($query); 
+		$stmt->execute();
+	
+		header("Location: account.php"); 		 
+	}
+	
+	catch(PDOException $ex) 
+	{ 
+		die("Failed to run query: " . $ex->getMessage()); 
+		header("Location: account.php");
+	}
 ?>
