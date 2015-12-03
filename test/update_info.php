7de44a8c5d8639d29c21ecec7eda28d258d05260
Jack Sarick <jacksarick@gmail.com>
2015-12-02 21:52:18 -0500

    fixed it

--- test/update_info.php
+++ test/update_info.php
@@ -1,54 +1,53 @@
 <?php 
 	require("common.php"); 
+	
+	if(
+		empty($_POST['first_name']) ||
+		empty($_POST['last_name'])  ||
+		empty($_POST['school'])
+	) { 
+		die("You missed a field"); 
+	}
 
-	// Make sure they did stuff
-	if(!empty($_POST)) { 
-		if(empty($_POST['firstname']) || empty($_POST['lastname']) || /*KEEP GOING*/) { 
-			die("You missed a field"); 
-		}
+	$_POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
 
-		//Prep variables
-		$email = $_SESSION['user']['email'];
-		$f_name = $_POST['first_name'];
-		$l_name = $_POST['last_name'];
-		$school = $_POST['school'];
-		$notes = $_POST['notes'];
+	$email = $_SESSION['user']['email'];
+	$f_name = $_POST['first_name'];
+	$l_name = $_POST['last_name'];
+	$school = $_POST['school'];
+	$notes = $_POST['notes'];
 
-		// Make query
-		$query = " 
-		REPLACE INTO info (
-			username,
-			first_name,
-			last_name,
-			plen_1, 
-			plen_2,
-			plen_3, 
-			school,
-			notes
-		) VALUES (
-			'$email',
-			'$f_name',
-			'$l_name',
-			'none', 
-			'none', 
-			'none',
-			'$school',
-			'$notes'
-		);";
+	$query = " 
+	REPLACE INTO info (
+		email,
+		first_name,
+		last_name,
+		plen_1, 
+		plen_2,
+		plen_3, 
+		school,
+		notes
+	) VALUES (
+		'$email',
+		'$f_name',
+		'$l_name',
+		'none', 
+		'none', 
+		'none',
+		'$school',
+		'$notes'
+	);";
 
-		// Execute said query
-		try { 
-			// These two statements run the query against your database table. 
-			$stmt = $db->prepare($query); 
-			$stmt->execute();
-		
-			header("Location: account.php"); 		 
-		}
-		
-		catch(PDOException $ex) 
-		{ 
-			die("Failed to run query: " . $ex->getMessage()); 
-			header("Location: account.php");
-		}
+	try { 
+		$stmt = $db->prepare($query); 
+		$stmt->execute();
+	
+		header("Location: account.php"); 		 
+	}
+	
+	catch(PDOException $ex) 
+	{ 
+		die("Failed to run query: " . $ex->getMessage()); 
+		header("Location: account.php");
 	}
 ?>
