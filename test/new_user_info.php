ba1bc0e9fb7ea7e3d0533bb668e177ef9abc50c7
Jack Sarick <jacksarick@gmail.com>
2015-12-02 20:28:34 -0500

    HOW DID I EVER FIGURE THIS SHIT OUT

--- test/new_user_info.php
+++ test/new_user_info.php
@@ -1,43 +1,41 @@
 <?php 
 	require("common.php"); 
 	
-	// if(empty($_POST['first_name']) ||empty($_POST['last_name'])||empty($_POST['school']))) { 
-	// 	die("You missed a field"); 
-	// }
+	if(empty($_POST['first_name']) ||empty($_POST['last_name'])||empty($_POST['school']))) { 
+		die("You missed a field"); 
+	}
 
-	// $query = " 
-	// INSERT INTO info (
-	// 	email,
-	// 	first_name,
-	// 	last_name,
-	// 	plen_1, 
-	// 	plen_2,
-	// 	plen_3, 
-	// 	school,
-	// 	notes
-	// ) VALUES (
-	// 	" . $_SESSION['user']['email'] . ","
-	// 	. $_POST['first_name'] . ""
-	// 	. $_POST['last_name'] . "
-	// 	'none', 
-	// 	'none', 
-	// 	'none', "
-	// 	. $_POST['school'] . ",",
-	// 	. $_POST['notes'] .
-	// ");";
+	$query = " 
+	INSERT INTO info (
+		email,
+		first_name,
+		last_name,
+		plen_1, 
+		plen_2,
+		plen_3, 
+		school,
+		notes
+	) VALUES (
+		" . $_SESSION['user']['email'] . ","
+		. $_POST['first_name'] . ""
+		. $_POST['last_name'] . "
+		'none', 
+		'none', 
+		'none', "
+		. $_POST['school'] . ",",
+		. $_POST['notes'] .
+	");";
 
-	var_dump($_SESSION['user']);
-
-	// try { 
-	// 	$stmt = $db->prepare($query); 
-	// 	$stmt->execute();
+	try { 
+		$stmt = $db->prepare($query); 
+		$stmt->execute();
 	
-	// 	header("Location: account.php"); 		 
-	// }
+		header("Location: account.php"); 		 
+	}
 	
-	// catch(PDOException $ex) 
-	// { 
-	// 	die("Failed to run query: " . $ex->getMessage()); 
-	// 	header("Location: account.php");
-	// }
+	catch(PDOException $ex) 
+	{ 
+		die("Failed to run query: " . $ex->getMessage()); 
+		header("Location: account.php");
+	}
 ?>
