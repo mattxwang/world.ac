82b13b42d71a20fe4d7298f6dccf837bad2711fb
Matthew Wang <matthewang.wang@gmail.com>
2015-12-05 21:56:04 -0500

    formats footers

--- footer.php
+++ footer.php
@@ -9,27 +9,27 @@ elseif($rando == 2){
 
 ?>
 
-  <nav class="navbar navbar-default navbar-fixed-bottom navbar-inverse navbar-footer hidden-xs" style="background-color:transparent; ">
-    <div class="container-fluid navbar-footer" style ="text-algin:center">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#footer-navbar" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt = "World Affairs Conference" height="25px"></img></a>
-      </div>
-      <div class="collapse navbar-collapse" id="footer-navbar" >
-        <ul class="nav navbar-nav">
-           <li><a href="#"><b>REGISTER</b></a></li>
-          <li><a href="about_us.php"><b>WAC TEAM</b></a></li>
-          <li><a href="contact_us.php"><b>CONTACT US</b></a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <p class="navbar-text" style ="font-size:11px; vertical-align:middle; display:table-cell;">Developed by this year's WAC web design team, <b><?php echo $teamNames; ?></b>
-        </ul>
-       </div>
-   </div>
-  </nav>
+<nav class="navbar navbar-default navbar-fixed-bottom navbar-inverse navbar-footer hidden-xs" style="background-color:transparent; ">
+  <div class="container-fluid navbar-footer" style ="text-algin:center">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#footer-navbar" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt = "World Affairs Conference" height="25px"></img></a>
+    </div>
+    <div class="collapse navbar-collapse" id="footer-navbar" >
+      <ul class="nav navbar-nav">
+        <li><a href="#"><b>REGISTER</b></a></li>
+        <li><a href="about_us.php"><b>WAC TEAM</b></a></li>
+        <li><a href="contact_us.php"><b>CONTACT US</b></a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <p class="navbar-text" style ="font-size:11px; vertical-align:middle; display:table-cell;">Developed by this year's WAC web design team, <b><?php echo $teamNames; ?></b>
+      </ul>
+    </div>
+  </div>
+</nav>
 
--- test/footer.php
+++ test/footer.php
@@ -1,77 +1,37 @@
 <?php
 $rando = rand(1,2);
-//$randa = rand(1,3);
 if($rando == 1){
-	$teamNames = "Matthew Wang and Jack Sarick";
-  /*
-	if($randa == 1){
-		$lteamNames = "Nick Elder, Derek Lam, and Kinton Cheung";
-	}
-	elseif($randa == 2){
-		$lteamNames = "Derek Lam, Kinton Cheung, and Nick Elder";
-	}
-	elseif($randa == 3){
-		$lteamNames = "Kinton Cheung, Nick Elder, and Derek Lam";
-	}
-  */
+  $teamNames = "Matthew Wang and <a href =\"http://jack.antinode.ca\">Jack Sarick</a>";
 }
 elseif($rando == 2){
-	$teamNames = "Jack Sarick and Matthew Wang";
-  /*
-	if($randa == 1){
-		$lteamNames = "Nick Elder, Derek Lam, and Kinton Cheung";
-	}
-	elseif($randa == 2){
-		$lteamNames = "Derek Lam, Kinton Cheung, and Nick Elder";
-	}
-	elseif($randa == 3){
-		$lteamNames = "Kinton Cheung, Nick Elder, and Derek Lam";
-	}
-  */
+  $teamNames = "<a href =\"http://jack.antinode.ca\">Jack Sarick</a> and Matthew Wang";
 }
 
 ?>
-<!--
-<div class = "row hidden-xs" style = "background-color:rgba(0,0,0,0.6);">
-  <div class = "col-md-2">
-    <a href="#">REGISTER</a>
+
+<nav class="navbar navbar-default navbar-fixed-bottom navbar-inverse navbar-footer hidden-xs" style="background-color:transparent; ">
+  <div class="container-fluid navbar-footer" style ="text-algin:center">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#footer-navbar" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt = "World Affairs Conference" height="25px"></img></a>
+    </div>
+    <div class="collapse navbar-collapse" id="footer-navbar" >
+      <ul class="nav navbar-nav">
+        <li><a href="#"><b>REGISTER</b></a></li>
+        <li><a href="about_us.php"><b>WAC TEAM</b></a></li>
+        <li><a href="contact_us.php"><b>CONTACT US</b></a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <p class="navbar-text" style ="font-size:11px; vertical-align:middle; display:table-cell;">Developed by this year's WAC web design team, <b><?php echo $teamNames; ?></b>
+      </ul>
+    </div>
   </div>
-  <div class = "col-md-2">
-    <a href="about_us.php">WAC TEAM</a>
-  </div>
-  <div class = "col-md-2">
-    <a href="contact_us.php">CONTACT US</a>
-  </div>
-  <div class = "col-md-6" style = "text-align:right">
-    Developed by this year's WAC web design team, <?php echo $teamNames; ?>
-  </div>
-</div>
--->
-  <nav class="navbar navbar-default navbar-fixed-bottom navbar-inverse navbar-footer hidden-xs" style="background-color:transparent; ">
-    <div class="container-fluid navbar-footer" style ="text-algin:center">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#footer-navbar" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt = "World Affairs Conference" height="25px"></img></a>
-      </div>
-      <div class="collapse navbar-collapse" id="footer-navbar" >
-        <ul class="nav navbar-nav">
-           <li><a href="#">REGISTER</a></li>
-          <li><a href="about_us.php">WAC TEAM</a></li>
-          <li><a href="contactus.php">CONTACT US</a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <p class="navbar-text" style ="font-size:11px; vertical-align:middle; display:table-cell;">Developed by this year's WAC web design team, <?php echo $teamNames; ?>
-          <!--</br>
-          With help from last year's team: <?php echo $lteamNames; ?></p>-->
-        </ul>
-       </div>
-   </div>
-  </nav>
+</nav>
 
 <!-- disabled for now
 <div class ="visible-xs vertical-align:bottom">
