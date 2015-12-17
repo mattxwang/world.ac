c27d5abc56220545de6d9077e053135b52f3b1ef
Matthew Wang <matthewang.wang@gmail.com>
2015-12-17 13:43:46 -0500

    welp it didn't work

--- about_us.php
+++ about_us.php
@@ -513,10 +513,8 @@ session_start();
 		include_once("footer.php")
 		?>
 
-		<script src="js/jquery.js"></script>
-		<script src="js/jquery.easing.min.js"></script>
+		<script src="js/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
-		<script src="js/scrolling-nav.js"></script>
 	</div>
 </body>
 </html>
