3551f89ccbcb7a929aaeb9ab7983135e22554b94
Matthew Wang <matthewang.wang@gmail.com>
2015-12-17 14:07:18 -0500

    woops

--- account.php
+++ account.php
@@ -346,7 +346,10 @@
 		<?php
 		include_once("footer.php")
 		?>
-		<script src="js/jquery.min.js"></script>
+		 <script src="js/jquery.js"></script>
+		<script src="js/jquery.easing.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
+		<script src="js/nav-collapse.js"></script>
+
 	</div>
 </body>
