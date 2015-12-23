b7be710b36752f98b15a7483d9abe7bc9756d51b
Matthew Wang <matthewang.wang@gmail.com>
2015-12-22 21:13:43 -0500

    adds more content

--- faq_test.php
+++ faq_test.php
@@ -45,7 +45,7 @@ session_start();
 				</div>
 				<div id="collapseGeneralWhat" class="panel-collapse collapse" role="tabpanel">
 					<div class="panel-body">
-					The World Affairs Conference (WAC) is Canada's oldest annual student-run current affairs conference. Annually held at Upper Canada College in Toronto, Canada, the World Affairs Conference connects over 800 students, with a common interest in current affairs, from across North America. A dedicated team of about 100 high school students from Upper Canada College, in coordination with Branksome Hall, voluntarily contribute their time for many months leading up to the day of WAC to ensure the success and positive experience for each delegate that attends the conference.
+					The World Affairs Conference (WAC) is Canada's oldest annual student-run current affairs conference. Annually held at Upper Canada College in Toronto, Canada, the World Affairs Conference connects over 1000 students, with a common interest in current affairs, from across North America. A dedicated team of about 100 high school students from Upper Canada College, in coordination with Branksome Hall, voluntarily contribute their time for many months leading up to the day of WAC to ensure the success and positive experience for each delegate that attends the conference.
 					</div>
 				</div>
 			</div>
@@ -66,6 +66,20 @@ session_start();
 			<div class="panel panel-default">
 				<div class="panel-heading" role="tab">
 					<h4 class="panel-title">
+					<a role="button" data-toggle="collapse" data-parent="#general" href="#collapseGeneralWho">
+					Who can attend WAC?
+					</a>
+					</h4>
+				</div>
+				<div id="collapseGeneralWho" class="panel-collapse collapse" role="tabpanel">
+					<div class="panel-body">
+					The Keynote Address is open to the Toronto public; anybody can attend. The Conference Day, in contrast, can only be attended by registered delegates and faculty advisors.
+					</div>
+				</div>
+			</div>
+			<div class="panel panel-default">
+				<div class="panel-heading" role="tab">
+					<h4 class="panel-title">
 					<a role="button" data-toggle="collapse" data-parent="#general" href="#collapseGeneralMedia">
 					Who do we contact for media information?
 					</a>
@@ -73,7 +87,7 @@ session_start();
 				</div>
 				<div id="collapseGeneralMedia" class="panel-collapse collapse" role="tabpanel">
 					<div class="panel-body">
-					For any information regarding media coverage please contact the Head of the Department of Registration and Media: Emmanuel Wong (emmanuel.wong@ucc.on.ca). If you require any WAC branding for your work please feel free to use the following provided logos: <a href="misc_files/WAC_Logos.zip"><b>download here.</b></a>
+					For any information regarding media coverage please contact the Head of the Department of Registration and Media: <b>Emmanuel Wong (emmanuel.wong@ucc.on.ca)</b>. If you require any WAC branding for your work please feel free to use the following provided logos: <a href="misc_files/WAC_Logos.zip"><b>download here.</b></a>
 					</div>
 				</div>
 			</div>
@@ -91,7 +105,7 @@ session_start();
 				</div>
 				<div id="collapseRegSchool" class="panel-collapse collapse" role="tabpanel">
 					<div class="panel-body">
-					To attend the World Affairs Conference this year, the registration form and means of payment need to be submitted by January 16th 2016. The registration information package, as well as the forms required to register are available for download, <b><a href="misc_files/reg_2015.pdf">here</a></b> and <b><a href ="misc_files/reg_2015_form.pdf">here</a></b>.
+					To attend the World Affairs Conference this year, the registration form and means of payment need to be submitted by <b>January 16th 2016</b>. The registration information package, as well as the forms required to register are available for download, <b><a href="misc_files/reg_2015.pdf">here</a></b> and <b><a href ="misc_files/reg_2015_form.pdf">here</a></b>.
 					</div>
 				</div>
 			</div>
@@ -119,34 +133,29 @@ session_start();
 				</div>
 				<div id="collapseRegDue" class="panel-collapse collapse" role="tabpanel">
 					<div class="panel-body">
-					Registration is due by mail on January 16th 2016. We may not be able to accept late registrants, so please ensure your forms are submitted promptly. 
+					Registration is due by mail by <b>January 16th 2016</b>. We may not be able to accept late registrants, so please ensure your forms are submitted promptly. 
+					</div>
+				</div>
+			</div>
+			<div class="panel panel-default">
+				<div class="panel-heading" role="tab">
+					<h4 class="panel-title">
+					<a role="button" data-toggle="collapse" data-parent="#reg" href="#collapseRegFood">
+					Can I inform WAC of special food accomodations?
+					</a>
+					</h4>
+				</div>
+				<div id="collapseRegFood" class="panel-collapse collapse" role="tabpanel">
+					<div class="panel-body">
+					Delegates can notify WAC of their food (or otherwise) accomodations through the <a href="account.php">account system</a>. 
+					We can guarantee that there will be vegetarian food that is available during the conference lunch.
+					Unfortunately, we are not certain that we can accomodate for every food accomodation.  We suggest delegates with special food accomodations either bring a pack lunch, or go to a nearby restaurant.
 					</div>
 				</div>
 			</div>
 		</div>
 
-		<div id = "reg">
-		<h2><a href="#reg">Registration</a></h2>
-		</div>
-		<div id="reg_student_cost">
-		<h3>Registration Price</h3>
-		<p>Registration for WAC 2016 is $50 CAD, per student. This should be submitted with delegate registration.</p>
-		</div>
-		<div id = "how_to_reg">
-			<h3>How can I register my school?</h3>
-			<p>To attend the world affairs conference you must submit your registration form and means of payment by the registration deadline. The registration information package, as well as the forms required to register are available for download, <b><a href="misc_files/reg_2015.pdf">here</a></b> and <b><a href ="misc_files/reg_2015_form.pdf">here</a></b>.</p>
-		</div>
-		<div id = "reg_who">
-			<h3>Who can attend WAC?</h3>
-			<p>WAC is open to anyone is currently in grades 9 to 12 with an interest in politics or current affairs. </p>
-		</div>
-		<div id = "reg_due">
-			<h3>When is the registration due?</h3>
-			<p>This, along with other information, will be sent out as we finish the registration package. </p>
-		
-			<p>The registration is due by mail on January 16th 2016. We may not be able to accept late registrants, so please ensure your forms are submitted promptly. </h3>
 	
-		</div>
 
 		<div id = "cday">
 		<h2><a href="#cday">Conference Day</a></h2>
@@ -173,9 +182,9 @@ session_start();
 
 	</div>
 
- <script src="js/jquery.js"></script>
-		<script src="js/jquery.easing.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-		<script src="js/nav-collapse.js"></script>
+	<script src="js/jquery.js"></script>
+	<script src="js/jquery.easing.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/nav-collapse.js"></script>
 </body>
 </html>
