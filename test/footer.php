99adb741b57eab557e5bb0a3101d71426d69e385
Matthew Wang <matthewang.wang@gmail.com>
2015-12-29 03:20:01 -0500

    resizing

--- test/footer.php
+++ test/footer.php
@@ -14,7 +14,7 @@ elseif($rando == 2){
 </br>
 <div class="containter-fluid" style = "background-color:#f8f8f8;color: black; padding: 5% 10% 5% 10%">
   <div class =  "row">
-    <div class = "col-md-3 vertical-line">
+    <div class = "col-md-2 vertical-line">
       <h4><b>Home</b></h4>
       <h6><a href="index.php">Homepage</a></h6>
       <h4><b>Registration Information</b></h4>
@@ -22,7 +22,7 @@ elseif($rando == 2){
       <h6><a href="reg_2016.pdf">Registration Package</a></h6>
       <h6><a href="reg_2016_form.pdf">Registration Form</a></h6>
     </div>
-    <div class = "col-md-3 vertical-line">
+    <div class = "col-md-2 vertical-line">
       <h4><b>Conference Information</b></h5>
       <h6><a href="keynote.php">Keynote Address</a></h6>
       <h6><a href="opening.php">Opening Panel</a></h6>
@@ -30,7 +30,7 @@ elseif($rando == 2){
       <h6><a href="opening.php">Closing Panel</a></h6>
       <h6><a href="plenaries.php">Schedule</a></h6>
     </div>
-    <div class = "col-md-3 vertical-line">
+    <div class = "col-md-2 vertical-line">
       <h4><b>About WAC</b></h5>
       <h6><a href="faq.php">FAQ</a></h6>
       <h6><a href="about_us.php">Meet the Team</a></h6>
@@ -41,7 +41,7 @@ elseif($rando == 2){
       </br>
       </div>
     </div>
-    <div class = "col-md-3">
+    <div class = "col-md-6">
       Developed by this year's WAC web design team, <?php echo $teamNames; ?>
       <p style="font-size: 8px">
       </br>
