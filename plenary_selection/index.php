14044f60acdd3d9e3b4a6b5e85a19661db8c03ea
malsf21 <matthewang.wang@gmail.com>
2016-01-08 09:39:27 -0500

    more touch ups on page

--- plenary_selection/index.php
+++ plenary_selection/index.php
@@ -12,57 +12,59 @@
 	</head>
 
 	<body>
-<script type="text/javascript">
-	//http://stackoverflow.com/questions/17973236/enable-or-disable-option-from-select
-	function toggleDisability(selectElement){
-		var arraySelects = document.getElementsByClassName('form-control');
-		var selectedOption = selectElement.selectedIndex;
-		for(var i=0; i<arraySelects.length; i++) {
-			if(arraySelects[i] == selectElement)
-			continue;
+		<script type="text/javascript">
+			//http://stackoverflow.com/questions/17973236/enable-or-disable-option-from-select
+			function toggleDisability(selectElement){
+				var arraySelects = document.getElementsByClassName('form-control');
+				var selectedOption = selectElement.selectedIndex;
+				for(var i=0; i<arraySelects.length; i++) {
+					if(arraySelects[i] == selectElement)
+					continue;
 
-			arraySelects[i].options[selectedOption].disabled = true;
-		}
-	}
-</script>
-		<form class="form-signin" action="plenary_selection_function.php" method="post">
-			<select class="form-control" id="plen_1" name="plen_1" required="">
-				<option value="A">China Plenary</option>
-				<option value="B">Drone Plenary</option>
-				<option value="C">Environmental Plenary</option>
-				<option value="D">Gender Issues Plenary</option>
-				<option value="E">Nuclear Plenary</option>
-				<option value="F">Refugee Plenary</option>
-			</select>
-			<select class="form-control" id="plen_2" name="plen_2" required="">
-				<option value="A">China Plenary</option>
-				<option value="B">Drone Plenary</option>
-				<option value="C">Environmental Plenary</option>
-				<option value="D">Gender Issues Plenary</option>
-				<option value="E">Nuclear Plenary</option>
-				<option value="F">Refugee Plenary</option>
-			</select>
-			<select class="form-control" id="plen_3" name="plen_3" required="">
-				<option value="A">China Plenary</option>
-				<option value="B">Drone Plenary</option>
-				<option value="C">Environmental Plenary</option>
-				<option value="D">Gender Issues Plenary</option>
-				<option value="E">Nuclear Plenary</option>
-				<option value="F">Refugee Plenary</option>
-			</select>
-			<select class="form-control" id="plen_4" name="plen_4" required="">
-				<option value="A">China Plenary</option>
-				<option value="B">Drone Plenary</option>
-				<option value="C">Environmental Plenary</option>
-				<option value="D">Gender Issues Plenary</option>
-				<option value="E">Nuclear Plenary</option>
-				<option value="F">Refugee Plenary</option>
-			</select>
-			<div class = "row">
-				<div class = "col-md-12">
-					<button class="btn btn-lg btn-primary btn-block" type="submit" value="Login">Register</button>
+					arraySelects[i].options[selectedOption].disabled = true;
+				}
+			}
+		</script>
+		<div class="container">
+			<form class="form-signin" action="plenary_selection_function.php" method="post">
+				<select onchange="toggleDisability(this);" class="form-control" id="plen_1" name="plen_1" required="">
+					<option value="A">China Plenary</option>
+					<option value="B">Drone Plenary</option>
+					<option value="C">Environmental Plenary</option>
+					<option value="D">Gender Issues Plenary</option>
+					<option value="E">Nuclear Plenary</option>
+					<option value="F">Refugee Plenary</option>
+				</select>
+				<select onchange="toggleDisability(this);" class="form-control" id="plen_2" name="plen_2" required="">
+					<option value="A">China Plenary</option>
+					<option value="B">Drone Plenary</option>
+					<option value="C">Environmental Plenary</option>
+					<option value="D">Gender Issues Plenary</option>
+					<option value="E">Nuclear Plenary</option>
+					<option value="F">Refugee Plenary</option>
+				</select>
+				<select onchange="toggleDisability(this);" class="form-control" id="plen_3" name="plen_3" required="">
+					<option value="A">China Plenary</option>
+					<option value="B">Drone Plenary</option>
+					<option value="C">Environmental Plenary</option>
+					<option value="D">Gender Issues Plenary</option>
+					<option value="E">Nuclear Plenary</option>
+					<option value="F">Refugee Plenary</option>
+				</select>
+				<select onchange="toggleDisability(this);" class="form-control" id="plen_4" name="plen_4" required="">
+					<option value="A">China Plenary</option>
+					<option value="B">Drone Plenary</option>
+					<option value="C">Environmental Plenary</option>
+					<option value="D">Gender Issues Plenary</option>
+					<option value="E">Nuclear Plenary</option>
+					<option value="F">Refugee Plenary</option>
+				</select>
+				<div class = "row">
+					<div class = "col-md-12">
+						<button class="btn btn-lg btn-primary btn-block" type="submit" value="Login">Register</button>
+					</div>
 				</div>
-			</div>
-		</form>
+			</form>
+		</div>
 	</body>
 </html>
