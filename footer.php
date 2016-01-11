340c90e0786521dc6b0f6520fcef31440e6b92dc
malsf21 <matthewang.wang@gmail.com>
2016-01-11 16:26:18 -0500

    fixes up font bugs (hopefully)

--- footer.php
+++ footer.php
@@ -15,15 +15,15 @@ elseif($rando == 2){
 <div class="containter-fluid" style = "background-color:#f8f8f8;color: black; padding: 2% 10% 1% 10%">
   <div class =  "row">
     <div class = "col-md-2 vertical-line">
-      <h4><b>Home</b></h4>
+      <h4>Home</h4>
       <h6><a href="index.php">Homepage</a></h6>
-      <h4><b>Registration Information</b></h4>
+      <h4>Registration Information</h4>
       <h6><a href="reg_how.php">How to Register</a></h6>
       <h6><a href="reg_2016.pdf">Registration Package</a></h6>
       <h6><a href="reg_2016_form.pdf">Registration Form</a></h6>
     </div>
     <div class = "col-md-2 vertical-line">
-      <h4><b>Conference Information</b></h5>
+      <h4>Conference Information</h4>
       <h6><a href="keynote.php">Keynote Address</a></h6>
       <h6><a href="opening.php">Opening Panel</a></h6>
       <h6><a href="plenaries.php">Plenaries</a></h6>
@@ -31,7 +31,7 @@ elseif($rando == 2){
       <h6><a href="construction.php">Schedule</a></h6>
     </div>
     <div class = "col-md-2 vertical-line">
-      <h4><b>About WAC</b></h5>
+      <h4>About WAC</h4>
       <h6><a href="faq.php">FAQ</a></h6>
       <h6><a href="about_us.php">Meet the Team</a></h6>
       <h6><a href="contact_us.php">Contact Us</a></h6>
@@ -42,11 +42,11 @@ elseif($rando == 2){
       </div>
     </div>
     <div class = "col-md-6" style="text-align: right;">
-      <h4><b>The World Affairs Conference Website</b></h4>
+      <h4>The World Affairs Conference Website</h4>
       Developed by <?php echo $teamNames; ?>
       </br>
       </br>
-      <h4><b>The World Affairs Conference 2016</b></h4>
+      <h4>The World Affairs Conference 2016</h4>
       February 1st 2016 | February 2nd 2016
       </br>
       <a href="http://ucc.on.ca">Upper Canada College</a>
