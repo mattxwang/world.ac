9e7553cc3a0010eb573e5d986c257b025ca8ca66
Matthew Wang <matthewang.wang@gmail.com>
2016-01-18 21:06:00 -0500

    adds container to alert

--- index.php
+++ index.php
@@ -26,9 +26,11 @@ session_start();
 	<body>
 		<?php include_once("import_info.php") ?>
 		<?php include_once("navbar.php")  ?>
+		<div class="container">
 		<div class="alert alert-dismissible alert-info fade in">
 			<button type="button" class="close" data-dismiss="alert">x</button>
 			<p><span class="glyphicon glyphicon-pushpin"></span> Hey there! We just announced the <a href="keynote.php"><b>Lionel Gelber Keynote Speaker</b>! Discover who will be joining the ranks of Edward Snowden, Ralph Nader, and more!</a></p>
+		</div>
 		</div>
 		<div style="line-height:80%; padding:4%; position:relative">
 			<div style="font-size:5vw;">
