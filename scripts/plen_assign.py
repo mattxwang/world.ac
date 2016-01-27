43efca73104480d054833d460f3209b2be7d0027
Jack Sarick <jacksarick@gmail.com>
2016-01-27 17:26:09 -0500

    I HATE PLENARIES

--- scripts/plen_assign.py
+++ scripts/plen_assign.py
@@ -17,7 +17,7 @@ for i in range(input("Amount of files\n>")):
 for file_name in files:
 	for line in open(file_name):
 		row = line.replace('"', '').split(",")
-		data[(row[1] + " " + row[2])] = [row[4], row[5], row[6], row[7]]
+		data[(row[1] + " " + row[2] + ", " + row[3])] = [row[4], row[5], row[6], row[7]]
 
 plenaries = {
 	"A": ["Nuclear Proliferation", "DCT", [2, 3]],
@@ -31,11 +31,11 @@ plenaries = {
 # Parse data
 for name, choice in data.iteritems():
 	try:
-		result[name] = [plenaries[choice[0]], plenaries[choice[1]], plenaries[choice[2]]]
+		result[name] = [plenaries[choice[0]], plenaries[choice[1]], plenaries[choice[2]], choice[3]]
 
 	# If the row is misformatted, or data is missing
 	except Exception, e:
-		abnormal.append(name)
+		result[name] = ["A", "B", "C", "D"]
 
 # Detect schedule conflicts
 for name, plens in result.iteritems():
@@ -48,61 +48,37 @@ for name, plens in result.iteritems():
 			result[name][2] = plenaries[data[name][3]]
 			# print "RESOLVED: ",
 			# print result[name]
+		else:
+			result[name].pop(3)
 
 	# anomaly detection
 	except Exception, e:
 		print "\nFookin hell m8. IDK what's wrong with: " + name + "\n"
-		# Execute the anomaly
-		result.pop(name)
 
-print result
+# print result
 
+# Totally snapped right here and hardcoded the plenary arrangement
+i_hate_python = {}
 
 # Legit schedule creation
-for name in result:
-	session = [result[name][0][2], result[name][1][2], result[name][2][2]]
+for user, rooms in result.iteritems():
+	session = [rooms[0][2], rooms[1][2], rooms[2][2]]
 
+	timing = {}
 
-	if session[0][0] in (session[1] + session[2]):
-		session[0].pop(0)
+	#12 12 23 || 23 23 12
+	if session[0] == session[1]:
+		timing = [rooms[0][0] + " in " + rooms[0][1], rooms[1][0] + " in " + rooms[1][1], rooms[2][0] + " in " + rooms[2][1]]
+		#23 23 12
+		if session[0] == [2,3]:
+			timing[::-1]
+	
+	#12 23 12 || 23 12 23
 	else:
-		session[0].pop(1)
+		timing = [rooms[0][0] + " in " + rooms[0][1], rooms[2][0] + " in " + rooms[2][1], rooms[1][0] + " in " + rooms[1][1]]
+		if session[0] == [2,3]:
+			timing[::-1]
 
-	if session[1][0] in (session[0] + session[2]):
-		session[1].pop(0)
-	else:
-		session[1].pop(1)
+	i_hate_python[user] = timing
 
-	if session[2][0] in (session[0] + session[1]):
-		session[2].pop(0)
-	else:
-		session[2].pop(1)
-
-	# Give session-state back
-	result[name][0][2] = session[0][0]
-	result[name][1][2] = session[1][0]
-	result[name][2][2] = session[2][0]
-
-print "#########################"
-
-#DOUBLE CHECK
-# Detect schedule conflicts
-# Detect schedule conflicts
-for name, plens in result.iteritems():
-	try:
-		# make list of plenary slots
-		session = plens[0][2] + plens[1][2] + plens[2][2]
-		# If they miss a slot (means they have a conflict)
-		if ((1 not in session) or (2 not in session) or (3 not in session)):
-			# reassign to their third to fourth
-			result[name][2] = plenaries[data[name][3]]
-			# print "RESOLVED: ",
-			# print result[name]
-
-	# anomaly detection
-	except Exception, e:
-		print "\nFookin hell m8. IDK what's wrong with: " + name + "\n"
-		# Execute the anomaly
-		result.pop(name)
-
-print result
+print i_hate_python
