58d0f4a41dfa6432676be2d6ef7c1b1efbfebadb
malsf21 <matthewang.wang@gmail.com>
2016-05-28 15:28:24 -0400

    cleans up branch to be more lightweight

--- 2016/test/test_email.php
+++
@@ -1,9 +0,0 @@
-<?php
-// the message
-$msg = "Please visit the following link to activate account: <a href='http://world.ac/activate.php?" + str_rot13("jack.sarick@ucc.on.ca") + "'/>";
-
-// send email
-mail("jack.sarick@ucc.on.ca","My subject",$msg);
-
-echo "It worked!";
-?>
