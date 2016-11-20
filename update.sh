691db1e0c863ca4e5315d5f3bf2e9ba0d18a134b
Jack Sarick <jacksarick@gmail.com>
2016-11-19 23:24:22 -0500

    Proper scripting

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd js
+curl https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js > bootstrap.min.js
+curl https://code.jquery.com/jquery-3.1.1.min.js > jquery.min.js
+curl https://raw.githubusercontent.com/HubSpot/tether/master/dist/js/tether.min.js > tether.min.js
