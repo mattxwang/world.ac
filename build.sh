b5d2b18049b7eb095d70c8db52e78eac3d7d06a7
Matthew Wang <matthewang.wang@gmail.com>
2016-11-21 12:22:09 -0500

    adds build script and travis swap to build script

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# A small script to "compile" all the work for the website. This is the production build.
+cd ${0%/*} # Go to this directory
+sass css/bootstrap/bootstrap.scss --style compressed > css/bootstrap.min.css # Sass -> CSS
+jekyll build # Build the site in the _site directory
