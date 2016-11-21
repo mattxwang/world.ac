b0344c1723a5ef047c1a8b42bba1c777656db265
Jack Sarick <jacksarick@gmail.com>
2016-11-20 23:09:39 -0500

    Setup script done

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Our dependancies
-BOOTSTRAP=https://github.com/twbs/bootstrap/archive/v4.0.0-alpha.5.zip
 JQUERY=https://code.jquery.com/jquery-3.1.1.min.js
 TETHER=https://raw.githubusercontent.com/HubSpot/tether/master/dist/js/tether.min.js
-FAVER=font-awesome-4.7.0 #Sorry, FA is not good at sticking to unix dev guidelines
-FONTAWSOME=http://fontawesome.io/assets/$FAVER.zip
+
+FA_VER=font-awesome-4.7.0
+FONTAWSOME=http://fontawesome.io/assets/$FA_VER.zip
+
+BS_VER=4.0.0-alpha.5
+BOOTSTRAP=https://codeload.github.com/twbs/bootstrap/zip/v$BS_VER
 
 # Make curl if it doesn't exist
 hash curl 2>/dev/null || { echo >&2 "curl not installed, building it from wget"; alias curl="wget -q -O - $1"; }
@@ -13,16 +16,17 @@ hash curl 2>/dev/null || { echo >&2 "curl not installed, building it from wget";
 # Helper functions
 uncurl() {
 	echo " . Downloading $2"
-	curl -s $1 > $2.zip
+	curl -s $1 > temp.zip
 	echo " . Unpacking $2"
-	unzip -q $2.zip
-	rm $2.zip
+	unzip -q temp.zip
+	rm temp.zip
 }
 
 mkcd() {
 	mkdir $1
 	cd $1
 }
+
 
 # Actual installation
 cd ${0%/*} # Go to this directory
@@ -38,15 +42,18 @@ echo " √ Javascript done!"
 
 # FontAwesome
 echo " - Starting FontAwesome"
-uncurl $FONTAWSOME fa-archive
-mv $FAVER fa-archive/
+uncurl $FONTAWSOME FontAwesome
+mv $FA_VER fa-archive
 mv fa-archive/fonts .
 mv fa-archive/css/font-awesome.min.css css/
 rm -r fa-archive
 echo " √ FontAwesome done!"
 
-# # Bootstrap
-# echo " - Starting Bootstrap"
-# uncurl $BOOTSTRAP bs-archive
-# mv bs-archive/scss css/bootstrap
-# mv bs-archive/dist/js/bootstrap.min.js js/
+# Bootstrap
+echo " - Starting Bootstrap"
+uncurl $BOOTSTRAP Bootstrap
+mv bootstrap-$BS_VER bs-archive
+mv bs-archive/scss css/bootstrap
+mv bs-archive/dist/js/bootstrap.min.js js/
+rm -r bs-archive
+echo " √ Bootstrap done!"
