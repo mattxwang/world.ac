cce71f2ae070d496f6cf5362eb8c6498749d8dfb
Jack Sarick <jacksarick@gmail.com>
2016-11-20 22:06:41 -0500

    Now has "insurance"

--- unsetup.sh
+++ unsetup.sh
@@ -7,9 +7,12 @@
 rm js/jquery.min.js
 rm js/tether.min.js
 
+rm fa-archive
 rm fa-archive.zip
 rm css/font-awesome.min.css
 rm -r fonts
 
+rm bs-archive
+rm bs-archive.zip
 rm css/bootstrap
 rm js/bootstrap.min.js
