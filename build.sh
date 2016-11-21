86c22ca1ea3a8a5372aa6ebe2bd7b5ffb1dda7d7
Jack Sarick <jacksarick@gmail.com>
2016-11-20 21:58:41 -0500

    Wow that is a lot of extra junk

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# A small script to "compile" all the work for the website
-cd ${0%/*} # Go to this directory
-sass css/bootstrap/bootstrap.scss --style compressed > css/bootstrap.min.css # Sass -> CSS
-jekyll serve || /usr/bin/python -m SimpleHTTPServer 4000 # Serve it
