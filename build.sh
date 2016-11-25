13b3702b92d99f65c31e9f569c5b83c26998699e
Matthew Wang <matthewang.wang@gmail.com>
2016-11-25 16:28:43 -0500

    reverts option bootstrap compile for now

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 
 # A small script to "compile" all the work for the website. This is the production build.
 
-# Getting the optional bootstrap compile variable
-BOOTSTRAP=${1:-bootstrap}
-
 cd ${0%/*} # Go to this directory
-sass css/bootstrap/$BOOTSTRAP.scss --style compressed > css/bootstrap.min.css # Sass -> CSS
+sass css/bootstrap/bootstrap.scss --style compressed > css/bootstrap.min.css # Sass -> CSS
 jekyll build # Build the site in the _site directory
--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Getting the optional bootstrap compile variable
-BOOTSTRAP=${1:-bootstrap}
-
 # A small script to "compile" all the work for the website
 cd ${0%/*} # Go to this directory
-sass css/bootstrap/$BOOTSTRAP.scss --style compressed > css/bootstrap.min.css # Sass -> CSS
+sass css/bootstrap/bootstrap.scss --style compressed > css/bootstrap.min.css # Sass -> CSS
 jekyll serve || /usr/bin/python -m SimpleHTTPServer 4000 # Serve it
