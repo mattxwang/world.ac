3beb580df95acdd16e2a59de326bd95c6699b04e
Matthew Wang <matthewang.wang@gmail.com>
2017-01-07 12:14:09 -0500

    bumps bootstrap version to alpha.6, updates styling accordingly

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ TETHER=https://raw.githubusercontent.com/HubSpot/tether/master/dist/js/tether.mi
 FA_VER=font-awesome-4.7.0
 FONTAWSOME=http://fontawesome.io/assets/$FA_VER.zip
 
-BS_VER=4.0.0-alpha.5
+BS_VER=4.0.0-alpha.6
 BOOTSTRAP=https://codeload.github.com/twbs/bootstrap/zip/v$BS_VER
 
 # Check to see if the user has all the tools
